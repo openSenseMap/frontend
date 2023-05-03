25e4dd59ee6664958fdc944611bc22a08a60c3e6
unknown <raabejonas@yahoo.de>
2023-05-03 15:13:25 +0200

    add zod validations

---
+++ app/lib/validations/campaign.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+export const campaignSchema = z.object({
+  title: z.string().min(3).max(52),
+  description: z.string(),
+  feature: z.any(),
+  keywords: z.array(z.string()),
+  priority: z.enum(["LOW", "MEDIUM", "HIGH", "URGENT"]),
+  location: z.string(),
+  participantCount: z.number().int().nonnegative(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  startDate: z.date(),
+  endDate: z.date().optional(),
+  phenomena: z.array(z.string()),
+  centerpoint: z.any(),
+});
