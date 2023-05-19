7af007058ba2865c7348cbf07ef5a462b16c714f
unknown <raabejonas@yahoo.de>
2023-05-19 16:13:35 +0200

    new packages

--- remix.config.js
+++ remix.config.js
@@ -4,5 +4,9 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: ["chartjs-adapter-date-fns", "maplibre-gl"]
+  serverDependenciesToBundle: [
+    "chartjs-adapter-date-fns",
+    "maplibre-gl",
+    "supercluster",
+  ],
 };
