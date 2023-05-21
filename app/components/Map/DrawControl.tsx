dee034576f44ebcf70d3eba1ea05b02c5a7a6d64
unknown <raabejonas@yahoo.de>
2023-05-21 14:09:28 +0200

    custom control

--- app/components/Map/DrawControl.tsx
+++ app/components/Map/DrawControl.tsx
@@ -1,7 +1,10 @@
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import MapboxDraw, { modes } from "@mapbox/mapbox-gl-draw";
 import { useControl } from "react-map-gl";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-
+import {
+  CircleMode,
+  DragCircleMode,
+} from "maplibre-gl-draw-circle";
 import type { MapRef, ControlPosition } from "react-map-gl";
 
 type DrawControlProps = ConstructorParameters<typeof MapboxDraw>[0] & {
@@ -12,13 +15,89 @@ type DrawControlProps = ConstructorParameters<typeof MapboxDraw>[0] & {
   onDelete?: (evt: { features: object[] }) => void;
 };
 
+let draw: MapboxDraw | null = null;
+
+class MyCustomControl {
+  containerCir: HTMLButtonElement | undefined;
+  containerRec: HTMLButtonElement | undefined;
+  map: any;
+  containerImgCir: HTMLImageElement | undefined;
+  containerImgRec: HTMLImageElement | undefined;
+  mainContainer: HTMLDivElement | undefined;
+  container: any;
+
+  onAdd(map: any) {
+    if (this.mainContainer) {
+      // If the buttons have already been created, return the existing container
+      return this.mainContainer;
+    }
+    this.containerCir = document.createElement("button");
+    this.containerRec = document.createElement("button");
+
+    this.map = map;
+
+    this.containerCir.onclick = () => {
+      const zoom = this.map.getZoom();
+      draw?.changeMode("drag_circle", {
+        initialRadiusInKm: 1 / Math.pow(2, zoom - 11),
+      });
+      draw?.delete("-96.5801808656236544.76489866786821");
+    };
+    this.containerCir.className =
+      "mapbox-gl-draw_ctrl-draw-btn my-custom-control-cir";
+    // this.containerCir.innerHTML = "&#x25EF";
+    this.containerImgCir = document.createElement("img");
+    this.containerImgCir.src =
+      " https://cdn-icons-png.flaticon.com/16/808/808569.png";
+    this.containerCir.appendChild(this.containerImgCir);
+
+    this.containerRec.onclick = () => {
+      draw?.changeMode("draw_rectangle");
+    };
+    this.containerRec.className =
+      "mapbox-gl-draw_ctrl-draw-btn my-custom-control-rec";
+    // this.containerRec.innerHTML = "&#9645";
+    this.containerRec.className = "h-[29px] w-[29px]";
+
+    this.containerImgRec = document.createElement("img");
+    this.containerImgRec.src =
+      "  https://cdn-icons-png.flaticon.com/16/7367/7367908.png";
+    this.containerRec.appendChild(this.containerImgRec);
+
+    this.mainContainer = document.createElement("div");
+
+    this.mainContainer.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+    this.mainContainer.appendChild(this.containerCir);
+    this.mainContainer.appendChild(this.containerRec);
+
+    return this.mainContainer;
+  }
+  onRemove() {
+    this.container.parentNode.removeChild(this.container);
+    this.map = undefined;
+  }
+}
 export default function DrawControl(props: DrawControlProps) {
+  const myCustomControl = new MyCustomControl();
   useControl<MapboxDraw>(
-    () => new MapboxDraw(props),
+    () => {
+      draw = new MapboxDraw({
+        ...props,
+        modes: {
+          ...modes,
+          draw_circle: CircleMode,
+          drag_circle: DragCircleMode,
+        },
+        // defaultMode: "drag_circle",
+      });
+      return draw;
+    },
     ({ map }: { map: MapRef }) => {
       props.onCreate && map.on("draw.create", props.onCreate);
       props.onUpdate && map.on("draw.update", props.onUpdate);
       props.onDelete && map.on("draw.delete", props.onDelete);
+
+      map.addControl(myCustomControl, "top-left");
     },
     ({ map }: { map: MapRef }) => {
       props.onCreate && map.off("draw.create", props.onCreate);
