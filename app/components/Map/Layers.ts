eaff8ff7d9dd91fe7bcea55256ff4aa98820f2ce
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-05-24 13:51:01 +0200

    Add filters to style unclustered points

--- app/components/Map/Layers.ts
+++ app/components/Map/Layers.ts
@@ -1,5 +1,15 @@
 import type { LayerProps } from "react-map-gl";
 
+// colors to use for the categories
+const colors = ["#fed976", "#feb24c", "#fd8d3c", "#fc4e2a"];
+
+// filters for classifying devices into three categories based on status
+export const deviceStatusFilter = {
+  active: ["==", ["get", "status"], "ACTIVE"],
+  inactive: ["==", ["get", "status"], "INACTIVE"],
+  old: ["==", ["get", "status"], "OLD"],
+};
+
 export const clusterLayer: LayerProps = {
   id: "osem-data",
   type: "circle",
@@ -28,8 +38,17 @@ export const unclusteredPointLayer: LayerProps = {
   source: "osem-data",
   filter: ["!", ["has", "point_count"]],
   paint: {
-    "circle-color": "#11b4da",
-    "circle-radius": 4,
+    "circle-color": [
+      "case",
+      deviceStatusFilter.active,
+      colors[0],
+      deviceStatusFilter.inactive,
+      colors[1],
+      deviceStatusFilter.old,
+      colors[2],
+      colors[3],
+    ],
+    "circle-radius": 8,
     "circle-stroke-width": 1,
     "circle-stroke-color": "#fff",
   },
--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useNavigate } from "@remix-run/react";
 import Map from "~/components/Map";
-import mapboxgl from "mapbox-gl/dist/mapbox-gl.css";
+import mapboxglcss from "mapbox-gl/dist/mapbox-gl.css";
 import Header from "~/components/header/Header";
 
 import type { LoaderArgs, LinksFunction } from "@remix-run/node";
@@ -16,12 +16,13 @@ import type {
 
 import { MapProvider } from "react-map-gl";
 import { Layer, Source } from "react-map-gl";
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 import { useHotkeys } from "@mantine/hooks";
 import type { FeatureCollection, Point } from "geojson";
 import {
   clusterCountLayer,
   clusterLayer,
+  deviceStatusFilter,
   unclusteredPointLayer,
 } from "~/components/Map/Layers";
 import type { Device } from "@prisma/client";
@@ -36,7 +37,7 @@ export const links: LinksFunction = () => {
   return [
     {
       rel: "stylesheet",
-      href: mapboxgl,
+      href: mapboxglcss,
     },
   ];
 };
@@ -68,9 +69,48 @@ export default function Explore() {
   ]);
 
   const data = useLoaderData<typeof loader>();
+  console.log("Explore - data: ", data);
   const navigate = useNavigate();
 
-  const mapRef = useRef<MapRef>(null);
+  const mapRef = useRef<MapRef | null>(null);
+  const mapRefCallback = useCallback((ref: MapRef | null) => {
+    if (ref !== null) {
+      //Set the actual ref we use elsewhere
+      mapRef.current = ref;
+      const map = ref;
+
+      const loadImage = () => {
+        if (!map.hasImage("cat")) {
+          //NOTE ref for adding local image instead
+          map.loadImage("/custom_marker.png", (error, image) => {
+            if (error || image === undefined) throw error;
+            map.addImage("cat", image);
+          });
+        }
+      };
+
+      loadImage();
+    }
+  }, []);
+
+  // const symbolLayer = {
+  //   id: "earthquake_label",
+  //   type: "symbol",
+  //   source: "osem-data",
+  //   filter: ["!=", "cluster", true],
+  //   layout: {
+  //     "text-field": [
+  //       "number-format",
+  //       ["get", "mag"],
+  //       { "min-fraction-digits": 1, "max-fraction-digits": 1 },
+  //     ],
+  //     "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+  //     "text-size": 10,
+  //   },
+  //   paint: {
+  //     "text-color": ["case", ["<", ["get", "mag"], 3], "black", "white"],
+  //   },
+  // };
 
   const onMapClick = (e: MapLayerMouseEvent) => {
     if (e.features && e.features.length > 0) {
@@ -103,7 +143,7 @@ export default function Explore() {
       <MapProvider>
         <Header devices={data.devices} />
         <Map
-          ref={mapRef}
+          ref={mapRefCallback}
           initialViewState={{ latitude: 7, longitude: 52, zoom: 2 }}
           interactiveLayerIds={["osem-data", "unclustered-point"]}
           onClick={onMapClick}
@@ -113,6 +153,16 @@ export default function Explore() {
             type="geojson"
             data={data.devices as FeatureCollection<Point, Device>}
             cluster={true}
+            clusterMaxZoom={14}
+            clusterRadius={50}
+            clusterProperties={
+              // keep separate counts for each status category in a cluster
+              {
+                active: ["+", ["case", deviceStatusFilter.active, 1, 0]],
+                inactive: ["+", ["case", deviceStatusFilter.inactive, 1, 0]],
+                old: ["+", ["case", deviceStatusFilter.old, 1, 0]],
+              }
+            }
           >
             <Layer {...clusterLayer} />
             <Layer {...clusterCountLayer} />
