642882c3f589926eebd37353e1adab6e233ec3f0
YouQam <qamazyousef@gmail.com>
2023-05-30 14:21:41 +0200

    center login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -80,7 +80,7 @@ export default function LoginPage() {
   }, [actionData]);
 
   return (
-    <div className="flex min-h-full flex-col justify-center">
+    <div className="flex min-h-full h-screen flex-col justify-center items-center">
       <div className="mx-auto w-full max-w-md px-8">
         <Form method="post" className="space-y-6" noValidate>
           <div>
