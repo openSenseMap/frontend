a66006dd28c22428b6225716d442cb9cdef700f4
fbruc03 <frebruc@gmail.com>
2023-06-09 17:24:37 +0700

    set features to full width

--- app/components/landing/features.tsx
+++ app/components/landing/features.tsx
@@ -7,7 +7,7 @@ type FeaturesProps = {
 export default function Features({data}: FeaturesProps) {
   return (
     <div id="features" className="flex h-full items-center justify-center">
-      <div className="w-5/6">
+      <div className="w-full">
         <div className="flex items-center justify-center pb-10">
           <p className="font-serif text-6xl font-black text-green-100 subpixel-antialiased dark:text-green-200">
             Features
@@ -18,7 +18,7 @@ export default function Features({data}: FeaturesProps) {
             return (
               <div
                 key={index}
-                className="flex items-center flex-col justify-center rounded-2xl border-4 border-solid border-green-100 p-4 text-gray-300 dark:border-green-200 dark:text-gray-100 md:m-4 dark:bg-gray-100/[rgba(217,217,217,0.1)]"
+                className="flex items-center text-center flex-col justify-center rounded-2xl border-4 border-solid border-green-100 p-4 text-gray-300 dark:border-green-200 dark:text-gray-100 md:m-4 dark:bg-gray-100/[rgba(217,217,217,0.1)]"
               >
                 <div className="pb-4 font-serif text-2xl font-extrabold text-green-100 subpixel-antialiased dark:text-green-200">
                   {feature.title}
