9eab363c8c145ed1c6ff5c3015c97c3763f4aaaa
unknown <raabejonas@yahoo.de>
2023-06-15 13:52:32 +0200

    add update schema for campaigns

--- app/lib/validations/campaign.ts
+++ app/lib/validations/campaign.ts
@@ -30,3 +30,26 @@ export const campaignSchema = z
     (data) => checkValidDates(data.startDate, data.endDate),
     "Start date must be earlier than End date."
   );
+
+export const campaignUpdateSchema = z.object({
+  title: z.string().min(3).max(52).optional(),
+  description: z.string().min(5).optional(),
+  feature: z.any().optional(),
+  priority: z.enum(["LOW", "MEDIUM", "HIGH", "URGENT"]).optional(),
+  country: z.string().optional(),
+  participantCount: z.number().int().nonnegative().optional(),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+  startDate: z.date().optional(),
+  endDate: z.date().optional(),
+  phenomena: z.array(z.string()).optional(),
+  exposure: z.enum(["UNKNOWN", "INDOOR", "MOBILE", "OUTDOOR"]).optional(),
+  hardware_available: z.boolean().optional(),
+  centerpoint: z.any().optional(),
+  requiredParticipants: z.number().int().nonnegative().optional(),
+  requiredSensors: z.number().int().nonnegative().optional(),
+});
+// .refine(
+//   (data) => checkValidDates(data.startDate, data.endDate),
+//   "Start date must be earlier than End date."
+// );
