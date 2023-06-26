d735bea30dcb8a9e68c8cd7e871b71969a5d7787
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-06-26 14:50:03 +0200

    Add auth to settings route and button to checkout profile

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import { SidebarNav } from "~/components/sidebar-nav";
 import { Separator } from "~/components/ui/separator";
+
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { requireUserId } from "~/session.server";
+import { Button } from "~/components/ui/button";
 
 const sidebarNavItems = [
   {
@@ -17,15 +22,27 @@ const sidebarNavItems = [
   },
 ];
 
+export async function loader({ request }: LoaderArgs) {
+  await requireUserId(request);
+  return json({});
+}
+
 export default function SettingsPage() {
   return (
     <>
       <div className="hidden space-y-6 p-10 pb-16 md:block">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-          <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
-          </p>
+        <div className="space-y-4">
+          <div className="flex justify-between">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+              <p className="text-muted-foreground">
+                Manage your account settings and set e-mail preferences.
+              </p>
+            </div>
+            <Button>
+              <Link to="/explore/profile/me">Go to your personal profile</Link>
+            </Button>
+          </div>
           <Separator className="my-6" />
           <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
             <aside className="lg:w-1/5">
