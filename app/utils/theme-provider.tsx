e51908a5e215534ad96c6df7bd66a89d5bd316dc
freds-dev <frebruc@gmail.com>
2023-07-09 15:04:28 +0800

    persist theme in cookie - still buggy

--- app/utils/theme-provider.tsx
+++ app/utils/theme-provider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import type { Dispatch, ReactNode, SetStateAction } from "react";
 
 enum Theme {
@@ -11,7 +11,31 @@ type ThemeContextType = [Theme | null, Dispatch<SetStateAction<Theme | null>>];
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState<Theme | null>(Theme.LIGHT);
+  const [theme, setTheme] = useState<Theme | null>(null);
+
+  // Update the stored theme preference when it changes
+  useEffect(() => {
+    if (theme) {
+      const cookieValue = encodeURIComponent(theme);
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 7); // Expires in 7 days
+      const cookieString = `theme=${cookieValue}; expires=${expires.toUTCString()}; path=/`;
+      document.cookie = cookieString;
+    }
+  }, [theme]);
+
+  // Hydrate the client-side state with the stored theme preference
+  useEffect(() => {
+    const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+    for (const cookie of cookies) {
+      const [name, value] = cookie.split("=");
+      if (name === "theme") {
+        const decodedValue = decodeURIComponent(value);
+        setTheme(decodedValue as Theme);
+        break;
+      }
+    }
+  }, []);
 
   return (
     <ThemeContext.Provider value={[theme, setTheme]}>
