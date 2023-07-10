5a2712b5c1d59be31ec7c24c4896aaaa5522ad0b
Felix Erdmann <hello@felixerdmann.com>
2023-07-10 15:45:32 +0200

    feat: simple color palette for mobile measurements

--- app/components/Map/mobile-box-layer.tsx
+++ app/components/Map/mobile-box-layer.tsx
@@ -7,19 +7,53 @@ import {
 } from "@turf/helpers";
 import type { MultiLineString, Point } from "geojson";
 import { useEffect, useState } from "react";
-import { Layer, Source } from "react-map-gl";
+import { Layer, Source, useMap } from "react-map-gl";
+import chroma from "chroma-js";
+import bbox from "@turf/bbox";
 
-export default function MobileBoxLayer({ sensor }: { sensor: Sensor }) {
+const LOW_COLOR = "#375F73";
+const HIGH_COLOR = "#B5F584";
+
+const FIT_PADDING = 50;
+const BOTTOM_BAR_HEIGHT = 400;
+
+export default function MobileBoxLayer({
+  sensor,
+  color,
+}: {
+  sensor: Sensor;
+  color?:
+    | mapboxgl.CirclePaint["circle-color"]
+    | mapboxgl.LinePaint["line-color"];
+}) {
   const [sourceData, setSourceData] = useState<GeoJSON.FeatureCollection>();
 
+  const { osem } = useMap();
+
   useEffect(() => {
-    const points = (
-      sensor.data! as unknown as {
-        value: String;
-        location?: number[];
-        createdAt: Date;
-      }[]
-    ).map((measurement) => point(measurement.location!));
+    const sensorData = sensor.data! as unknown as {
+      value: String;
+      location?: number[];
+      createdAt: Date;
+    }[];
+
+    // create color palette from min and max values
+    const minValue = Math.min(...sensorData.map((d) => Number(d.value)));
+    const maxValue = Math.max(...sensorData.map((d) => Number(d.value)));
+
+    const palette = chroma
+      .scale([LOW_COLOR, HIGH_COLOR])
+      .domain([minValue, maxValue]);
+
+    // generate points from the sensor data
+    // apply color from palette
+    const points = sensorData.map((measurement) =>
+      point(measurement.location!, {
+        value: Number(measurement.value),
+        createdAt: new Date(measurement.createdAt),
+        color: palette(Number(measurement.value)).hex(),
+      })
+    );
 
     if (points.length === 0) return;
 
@@ -32,6 +66,20 @@ export default function MobileBoxLayer({ sensor }: { sensor: Sensor }) {
     );
   }, [sensor.data]);
 
+  // fit the map to the bounds of the data
+  useEffect(() => {
+    if (!osem || !sourceData) return;
+    const [x1, y1, x2, y2] = bbox(sourceData);
+    osem?.fitBounds([x1, y1, x2, y2], {
+      padding: {
+        top: FIT_PADDING,
+        bottom: BOTTOM_BAR_HEIGHT + FIT_PADDING,
+        left: FIT_PADDING,
+        right: FIT_PADDING,
+      },
+    });
+  }, [osem, sourceData]);
+
   if (!sourceData) return null;
 
   return (
@@ -39,20 +87,24 @@ export default function MobileBoxLayer({ sensor }: { sensor: Sensor }) {
       <Layer
         id="box-layer-point"
         source="box-source"
+        filter={["==", "$type", "Point"]}
         type="circle"
         paint={{
-          "circle-color": "red",
-          "circle-radius": 4,
+          "circle-color": ["get", "color"],
+          "circle-radius": 5,
         }}
       />
       <Layer
         id="box-layer-line"
         source="box-source"
         type="line"
+        filter={["==", "$type", "LineString"]}
         paint={{
-          "line-color": "red",
+          "line-color": color || "#333",
           "line-width": 2,
+          "line-opacity": 0.7,
         }}
+        beforeId="box-layer-point"
       />
     </Source>
   );
