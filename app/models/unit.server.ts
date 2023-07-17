638d6cfea5f50e37fe1345bbec49e9c16e38c667
freds-dev <frebruc@gmail.com>
2023-07-17 17:27:50 +0800

    add units

---
+++ app/models/unit.server.ts
@@ -0,0 +1,12 @@
+export async function getUnits() {
+  const response = await fetch("https://api.sensors.wiki/units");
+  const jsonData = await response.json();
+  return jsonData;
+}
+
+export type Unit = {
+  id: number;
+  slug: string;
+  name: string;
+  notation: string;
+};
