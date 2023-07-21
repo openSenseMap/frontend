9d7cf5da6a01d47df25938f8efc1d6f41d6f65e0
unknown <raabejonas@yahoo.de>
2023-07-21 16:21:10 +0200

    style search responsive

--- app/components/campaigns/overview/search.tsx
+++ app/components/campaigns/overview/search.tsx
@@ -4,9 +4,9 @@ import { Button } from "~/components/ui/button";
 export default function SearchField() {
   const [searchParams] = useSearchParams();
   return (
-    <Form>
+    <Form className="mx-auto mt-5 grid w-1/3 flex-wrap items-end gap-2 gap-x-4 gap-y-2 sm:flex">
       <input
-        className="focus:ring-blue-400 mx-auto mt-5 w-1/3 rounded-md border border-gray-300 px-4 py-2 text-center text-lg focus:border-transparent focus:outline-none focus:ring-2"
+        className="focus:ring-blue-400 flex-grow rounded-md border border-gray-300 px-4 py-2 text-center text-lg focus:border-transparent focus:outline-none focus:ring-2"
         type="text"
         name="search"
         id="search"
@@ -20,7 +20,13 @@ export default function SearchField() {
         //   })
         // }
       />
-      <Button type="submit">Search </Button>
+      <Button
+        type="submit"
+        className="items-center justify-center py-2"
+        variant="destructive"
+      >
+        Search{" "}
+      </Button>
     </Form>
   );
 }
