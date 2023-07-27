90d5871aa491ca0f7825804a355c329a932ec468
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-07-27 14:51:07 +0200

    Add stored procedure and cron schedule to migration

--- prisma/migrations/20230727095644_add_pg_cron_extension/migration.sql
+++ prisma/migrations/20230727095644_add_pg_cron_extension/migration.sql
@@ -1,2 +1,17 @@
 -- CreateExtension
 CREATE EXTENSION IF NOT EXISTS "pg_cron";
+
+-- Stored procedure to update Sensor status
+CREATE OR REPLACE PROCEDURE update_sensor_status()
+LANGUAGE SQL
+AS $$
+	UPDATE "Sensor"
+	SET status = CASE
+		WHEN (SELECT max(time) FROM "Measurement" WHERE "Sensor".id = "Measurement"."sensorId") > now() - interval '7 days' THEN 'ACTIVE'::"Status"
+		WHEN (SELECT max(time) FROM "Measurement" WHERE "Sensor".id = "Measurement"."sensorId") > now() - interval '30 days' AND (SELECT max(time) FROM "Measurement" WHERE "Sensor".id = "Measurement"."sensorId") < now() - interval '8 days' THEN 'INACTIVE'::"Status"
+		ELSE 'OLD'::"Status"
+	END;
+$$;
+
+-- Call stored procedure every 5 minutes
+SELECT cron.schedule('sensor-update-status', '*/5 * * * *', 'CALL update_sensor_status()');
