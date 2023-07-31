ccbe10649cbb976b25000a0b344cd38ac0f492c5
Felix Erdmann <hello@felixerdmann.com>
2023-07-31 18:16:20 +0200

    (Mobile) Search bar (#66)
    * feat: update search bar
    
    * feat: close search after selecting result using context
    
    * chore: remove unused overlay-search
    
    * feat: mobile search bar
    
    * chore: fix import
    
    * fix: type errors
    
    * feat: enhance state of search
    
    * feat: add inital i18n
    
    * fix: import duplicate
    
    * feat: handle input focus
    
    * fix issue #39: `Ctrl`+`Number` selection of result (#90)
    
    * feat: collapse animate search bar
    
    * chore: remove yarn.lock
    
    * chore: remove unused files
    
    ---------
    
    Co-authored-by: fab-scm <82449462+fab-scm@users.noreply.github.com>

--- app/components/search/search-list.tsx
+++ app/components/search/search-list.tsx
@@ -1,57 +1,29 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useContext } from "react";
 import { useMap } from "react-map-gl";
 import { useNavigate, useSearchParams } from "@remix-run/react";
 
 import SearchListItem from "./search-list-item";
 import { goTo } from "~/lib/search-map-helper";
+import useKeyboardNav from "../header/nav-bar/use-keyboard-nav";
+import { NavbarContext } from "../header/nav-bar";
 import { Cpu, Globe, MapPin } from "lucide-react";
 
 interface SearchListProps {
   searchResultsLocation: any[];
   searchResultsDevice: any[];
-  setShowSearch: (data: boolean) => void;
 }
 
 export default function SearchList(props: SearchListProps) {
   const { osem } = useMap();
   const navigate = useNavigate();
+  const { setOpen } = useContext(NavbarContext);
 
-  const useKeyPress = function (targetKey: string) {
-    const [keyPressed, setKeyPressed] = useState(false);
+  const { cursor, setCursor, enterPress, controlPress } = useKeyboardNav(
+    0,
+    0,
+    props.searchResultsDevice.length + props.searchResultsLocation.length
+  );
 
-    useEffect(() => {
-      const downHandler = ({ key }: { key: string }) => {
-        if (key === targetKey) {
-          setKeyPressed(true);
-        }
-      };
-
-      const upHandler = ({ key }: { key: string }) => {
-        if (key === targetKey) {
-          setKeyPressed(false);
-        }
-      };
-
-      window.addEventListener("keydown", downHandler);
-      window.addEventListener("keyup", upHandler);
-
-      return () => {
-        window.removeEventListener("keydown", downHandler);
-        window.removeEventListener("keyup", upHandler);
-      };
-    }, [targetKey]);
-
-    return keyPressed;
-  };
-
-  const downPress = useKeyPress("ArrowDown");
-  const upPress = useKeyPress("ArrowUp");
-  const enterPress = useKeyPress("Enter");
-  const controlPress = useKeyPress("Control");
-  const [cursor, setCursor] = useState(0);
-
-  var searchResultsDeviceIndex = -1;
-  var searchResultsLocationIndex = props.searchResultsDevice.length - 1;
   var length =
     props.searchResultsDevice.length + props.searchResultsLocation.length;
   var searchResultsAll = props.searchResultsDevice.concat(
@@ -68,36 +40,63 @@ export default function SearchList(props: SearchListProps) {
       (searchParams.size > 0 ? "?" + searchParams.toString() : "")
   );
 
+  const handleNavigate = useCallback(
+    (result: any) => {
+      return (
+        (result.type === "device"
+          ? `/explore/${result.deviceId}`
+          : "/explore") +
+        // @ts-ignore
+        (searchParams.size > 0 ? "?" + searchParams.toString() : "")
+      );
+    },
+    [searchParams]
+  );
+
+  const setShowSearchCallback = useCallback(
+    (state: boolean) => {
+      setOpen(state);
+    },
+    [setOpen]
+  );
+
+  const handleDigitPress = useCallback(
+    (event: any) => {
+      if (
+        typeof Number(event.key) === "number" &&
+        Number(event.key) <= length &&
+        controlPress
+      ) {
+        event.preventDefault();
+        setCursor(Number(event.key) - 1);
+        goTo(osem, searchResultsAll[Number(event.key) - 1]);
+        setTimeout(() => {
+          setShowSearchCallback(false);
+          navigate(handleNavigate(searchResultsAll[Number(event.key) - 1]));
+        }, 500);
+      }
+    },
+    [
+      controlPress,
+      length,
+      navigate,
+      osem,
+      searchResultsAll,
+      setCursor,
+      setShowSearchCallback,
+      handleNavigate,
+    ]
+  );
+
   useEffect(() => {
     setSelected(searchResultsAll[cursor]);
   }, [cursor, searchResultsAll]);
 
   useEffect(() => {
-    if (selected.type === "device") {
-      // @ts-ignore
-      setNavigateTo(`/explore/${selected.deviceId}` + (searchParams.size > 0 ? "?" + searchParams.toString() : ""));
-    } else if (selected.type === "location") {
-      // @ts-ignore
-      setNavigateTo("/explore" + (searchParams.size > 0 ? "?" + searchParams.toString() : ""));
-    }
-    console.log(navigateTo);
-  }, [selected, searchParams, navigateTo]);
+    const navigate = handleNavigate(selected);
+    setNavigateTo(navigate);
+  }, [selected, handleNavigate]);
 
-  const setShowSearchCallback = useCallback((state: boolean) => {
-    props.setShowSearch(state);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    if (length !== 0 && downPress) {
-      setCursor((prevState) => (prevState < length - 1 ? prevState + 1 : 0));
-    }
-  }, [downPress, length]);
-  useEffect(() => {
-    if (length !== 0 && upPress) {
-      setCursor((prevState) => (prevState > 0 ? prevState - 1 : length - 1));
-    }
-  }, [upPress, length]);
   useEffect(() => {
     if (length !== 0 && enterPress) {
       goTo(osem, selected);
@@ -106,27 +105,13 @@ export default function SearchList(props: SearchListProps) {
     }
   }, [
     enterPress,
-    length,
     osem,
     navigate,
     selected,
     setShowSearchCallback,
     navigateTo,
+    length,
   ]);
-
-  const handleDigitPress = (event: any) => {
-    if (
-      typeof Number(event.key) === "number" &&
-      Number(event.key) <= length &&
-      event.ctrlKey
-    ) {
-      event.preventDefault();
-      setCursor(Number(event.key) - 1);
-      goTo(osem, selected);
-      setTimeout(() => {setShowSearchCallback(false); navigate(navigateTo);}, 500);
-      
-    }
-  };
 
   useEffect(() => {
     // attach the event listener
@@ -140,45 +125,43 @@ export default function SearchList(props: SearchListProps) {
 
   return (
     <div className="w-full overflow-visible rounded-[1.25rem] bg-white pb-2">
-      {props.searchResultsDevice.length > 0 ? (
+      {props.searchResultsDevice.length > 0 && (
         <hr className="solid mx-2 mb-2 border-t-2" />
-      ) : null}
-      {props.searchResultsDevice.map((device: any) => {
-        searchResultsDeviceIndex++;
-        return (
-          <SearchListItem
-            key={device.deviceId}
-            index={searchResultsDeviceIndex}
-            active={searchResultsDeviceIndex === cursor}
-            type="device"
-            item={device}
-            icon={Cpu}
-            setShowSearch={props.setShowSearch}
-            setCursor={setCursor}
-            controlPress={controlPress}
-          />
-        );
-      })}
-      {props.searchResultsLocation.length > 0 ? (
+      )}
+      {props.searchResultsDevice.map((device: any, i) => (
+        <SearchListItem
+          key={device.deviceId}
+          index={i}
+          active={i === cursor}
+          name={device.display_name}
+          icon={Cpu}
+          controlPress={controlPress}
+          onMouseEnter={() => setCursor(i)}
+          onClick={() => {
+            goTo(osem, device);
+            setShowSearchCallback(false);
+            navigate(navigateTo);
+          }}
+        />
+      ))}
+      {props.searchResultsLocation.length > 0 && (
         <hr className="solid m-2 border-t-2" />
-      ) : null}
-      {props.searchResultsLocation.map((location: any) => {
-        searchResultsLocationIndex++;
+      )}
+      {props.searchResultsLocation.map((location: any, i) => {
         return (
           <SearchListItem
             key={location.id}
-            index={searchResultsLocationIndex}
-            active={searchResultsLocationIndex === cursor}
-            type="location"
-            item={location}
-            icon={
-              location.place_type.includes("country")
-                ? Globe
-                : MapPin
-            }
-            setShowSearch={props.setShowSearch}
-            setCursor={setCursor}
+            index={i + props.searchResultsDevice.length}
+            active={i + props.searchResultsDevice.length === cursor}
+            name={location.place_name}
+            icon={location.place_type.includes("country") ? Globe : MapPin}
             controlPress={controlPress}
+            onMouseEnter={() => setCursor(i + props.searchResultsDevice.length)}
+            onClick={() => {
+              goTo(osem, location);
+              setShowSearchCallback(false);
+              navigate(navigateTo);
+            }}
           />
         );
       })}
