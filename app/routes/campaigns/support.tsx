404d879a288b58aeddf98b5d350f8668f797c8ca
unknown <raabejonas@yahoo.de>
2023-08-03 08:23:57 +0200

    remove stuff

--- app/routes/campaigns/support.tsx
+++ app/routes/campaigns/support.tsx
@@ -58,7 +58,6 @@ export default function Support() {
 
   return (
     <div className="flex min-h-full flex-col justify-center">
-      <h2 className="m-2 text-lg font-bold">Support</h2>
       <div className="w-full text-center">
         <p>
           Nutzen Sie dieses Formular um technische Unterstützung bei Problemen
@@ -124,32 +123,6 @@ export default function Support() {
               className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
             />
             {/* {actionData?.errors?.detailed-description && (
-                    <div className="text-red-700 pt-1" id="email-error">
-                      {actionData.errors.email}
-                    </div>
-                  )} */}
-          </div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Bestätigungsemail an
-          </label>
-          <div className="mt-1">
-            <input
-              // ref={emailRef}
-              id="email"
-              required
-              autoFocus={true}
-              maxLength={180}
-              name="email"
-              type="email"
-              autoComplete="email"
-              // aria-invalid={actionData?.errors?.email ? true : undefined}
-              aria-describedby="email-error"
-              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-            />
-            {/* {actionData?.errors?.email && (
                     <div className="text-red-700 pt-1" id="email-error">
                       {actionData.errors.email}
                     </div>
@@ -251,7 +224,7 @@ export default function Support() {
           {/* <input type="hidden" name="redirectTo" value={redirectTo} /> */}
           <button
             type="submit"
-            className="hover:bg-blue-600 focus:bg-blue-400 w-full  rounded bg-blue-500 py-2 px-4 text-white"
+            className="hover:bg-blue-600 focus:bg-blue-400 w-full  rounded bg-blue-500 px-4 py-2 text-white"
           >
             Abschicken
           </button>
