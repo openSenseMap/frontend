1bf6a7b113aa068d129b10389d5361f939a66b17
freds-dev <frebruc@gmail.com>
2023-08-13 15:28:31 +0200

    add functional date picker and fix date formats

--- app/models/measurement.server.ts
+++ app/models/measurement.server.ts
@@ -1,6 +1,58 @@
 import { prisma } from "~/db.server";
 
-export function getMeasurement(sensorId: string, aggregation: string) {
+// This function retrieves measurements from the database based on the provided parameters.
+export function getMeasurement(
+  sensorId: string,
+  aggregation: string,
+  startDate?: Date,
+  endDate?: Date
+) {
+  // If both start date and end date are provided, filter measurements within the specified time range.
+  if (startDate && endDate) {
+    // Check the aggregation level for measurements and fetch accordingly.
+    if (aggregation === "15m") {
+      return prisma.measurements_15min.findMany({
+        where: {
+          sensorId: sensorId,
+          time: {
+            gte: startDate,
+            lte: endDate,
+          },
+        },
+        orderBy: {
+          time: "desc",
+        },
+      });
+    } else if (aggregation === "1d") {
+      return prisma.measurements_1day.findMany({
+        where: {
+          sensorId: sensorId,
+          time: {
+            gte: startDate,
+            lte: endDate,
+          },
+        },
+        orderBy: {
+          time: "desc",
+        },
+      });
+    }
+    // If aggregation is not specified or different from "15m" and "1d", fetch default measurements.
+    return prisma.measurement.findMany({
+      where: {
+        sensorId: sensorId,
+        time: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      orderBy: {
+        time: "desc",
+      },
+    });
+  }
+
+  // If only aggregation is provided, fetch measurements without considering time range.
   if (aggregation === "15m") {
     return prisma.measurements_15min.findMany({
       where: {
@@ -21,6 +73,7 @@ export function getMeasurement(sensorId: string, aggregation: string) {
     });
   }
 
+  // If neither start date nor aggregation are specified, fetch default measurements with a limit of 5000.
   return prisma.measurement.findMany({
     take: 5000,
     where: {
