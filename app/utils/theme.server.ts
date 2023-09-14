70b9df2de0cb6b06caf54255c5c4ab3cf0b2fa06
freds-dev <frebruc@gmail.com>
2023-09-14 16:12:17 +0200

    sign cookies and add refactored theme-provider

--- app/utils/theme.server.ts
+++ app/utils/theme.server.ts
@@ -4,16 +4,15 @@ import { isTheme } from "./theme-provider";
 import type { Theme } from "./theme-provider";
 
 // Make use to set the environment variable SESSION_SECRET before running the code
-//const sessionSecret = process.env.SESSION_SECRET ?? "DEFAULT_SECRET";
+const sessionSecret = process.env.SESSION_SECRET ?? "P0JPnyms9A";
 
 const themeStorage = createCookieSessionStorage({
   cookie: {
     name: "tailwind_theme",
-    //secure: true,
-    //secrets: [sessionSecret],
+    secure: true,
+    secrets: [sessionSecret],
     sameSite: "lax",
     path: "/",
-    //httpOnly: true,
   },
 });
 
