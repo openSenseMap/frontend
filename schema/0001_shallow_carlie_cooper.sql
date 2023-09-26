d413ef3d18d52a30f2b06c1a68374b4c384d9afa
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-09-26 13:01:52 +0200

    Remove old drizzle stuff

--- schema/0001_shallow_carlie_cooper.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "password" ALTER COLUMN "hash" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "profile" ALTER COLUMN "username" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "email" SET NOT NULL;
