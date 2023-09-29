648d5bfa6a21ebb932ebc1291bda92b0350b4d7f
freds-dev <frebruc@gmail.com>
2023-09-29 10:24:12 +0200

    update remix config `serverDependenciesToBundle`

--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,6 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: ["chartjs-adapter-date-fns", "maplibre-gl"],
+  serverDependenciesToBundle: ["chartjs-adapter-date-fns", "maplibre-gl", "supercluster", "use-supercluster", "kdbush"],
   serverModuleFormat: "cjs",
 };
