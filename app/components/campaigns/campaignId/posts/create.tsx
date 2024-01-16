7c0a27fcd8b9a36e377b20fb8bd6c530f3ff83e4
jona159 <raabejonas@yahoo.de>
2024-01-16 23:38:53 +0100

    create thread component

---
+++ app/components/campaigns/campaignId/posts/create.tsx
@@ -0,0 +1,31 @@
+import { Form } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+
+type Props = {
+  loggedIn: boolean;
+};
+
+export default function CreateThread({ loggedIn }: Props) {
+  if (loggedIn) {
+    return (
+      <Form method="post" className="mt-8 w-full">
+        <span>Create new Thread</span>
+        <div className="flex w-full justify-between">
+          <input type="text" name="title" id="title"></input>
+          <input type="text" name="content" id="content"></input>
+          <Button
+            className="bg-blue-700 text-white"
+            variant="outline"
+            type="submit"
+            name="_action"
+            value="CREATE_POST"
+          >
+            Create
+          </Button>
+        </div>
+      </Form>
+    );
+  } else {
+    return <span>Login to create a Thread</span>;
+  }
+}
