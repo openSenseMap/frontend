918703442c46c15b56e57379b6b61d6b78b21d87
jona159 <raabejonas@yahoo.de>
2024-01-20 14:05:17 +0100

    styling, replies`

--- app/components/campaigns/campaignId/posts/index.tsx
+++ app/components/campaigns/campaignId/posts/index.tsx
@@ -1,7 +1,9 @@
 import { Form } from "@remix-run/react";
 import { ReplyIcon } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { ClientOnly } from "remix-utils";
 import { Button } from "~/components/ui/button";
+import { MarkdownEditor } from "~/markdown.client";
 import { Comment, Post } from "~/schema";
 
 type Props = {
@@ -13,6 +15,9 @@ interface ShowComments {
 }
 
 export default function ListPosts({ posts }: Props) {
+  const replyRef = useRef();
+  const [reply, setReply] = useState<string | undefined>("");
+
   const initialState: ShowComments = posts.reduce((acc: ShowComments, post) => {
     acc[post.id] = false;
     return acc;
@@ -33,66 +38,96 @@ export default function ListPosts({ posts }: Props) {
         {posts.map((p) => {
           return (
             <>
-              <li className="flex w-full justify-between" key={p.id}>
-                <span>{p.content}</span>
-                <Form method="post">
-                  <input
-                    className="hidden"
-                    value={p.id}
-                    name="postId"
-                    id="postId"
-                  />
-                </Form>
+              <li
+                className="justify-conter flex w-full flex-col items-center p-4"
+                key={p.id}
+              >
+                <div className="mb-4 flex w-3/4 items-center justify-between p-4">
+                  <div className="flex flex-col">
+                    <span>{p.content}</span>
+
+                    {p.comment && p.comment.length > 0 && (
+                      <Button
+                        variant="ghost"
+                        className="flex gap-2"
+                        onClick={() => handleReplyClick(p.id)}
+                      >
+                        <ReplyIcon className="h-4 w-4" />
+                        {p.comment.length} Replies
+                      </Button>
+                    )}
+                    {showComments[p.id] && (
+                      <>
+                        {p.comment &&
+                          p.comment.map((c: Comment) => {
+                            return <span>{c.content}</span>;
+                          })}
+                        {/* <Form method="post" className="w-full">
+                          <input
+                            className="hidden"
+                            id="postId"
+                            name="postId"
+                            value={p.id}
+                          /> */}
+                        <ClientOnly>
+                          {() => (
+                            <div className="container mt-4 overflow-auto">
+                              <MarkdownEditor
+                                textAreaRef={replyRef}
+                                comment={reply}
+                                setComment={setReply}
+                              />
+                              <div className="w-100 border-blue-grey relative flex justify-between rounded-b-lg border border-l border-r border-t-0 px-2 py-1 shadow-md">
+                                <span className="text-gray text-xs leading-4">
+                                  Bild hinzufügen
+                                </span>
+                                <span className="text-gray text-xs leading-4">
+                                  Markdown unterstützt
+                                </span>
+                              </div>
+                              <Form method="post">
+                                <input
+                                  className="hidden"
+                                  value={p.id}
+                                  name="postId"
+                                  id="postId"
+                                />
+                                <textarea
+                                  className="hidden"
+                                  value={reply}
+                                  name="content"
+                                  id="content"
+                                ></textarea>
+                                <Button
+                                  name="_action"
+                                  value="PUBLISH"
+                                  type="submit"
+                                  className="float-right mt-2 bg-blue-700 text-white"
+                                >
+                                  Veröffentlichen
+                                </Button>
+                              </Form>
+                            </div>
+                          )}
+                        </ClientOnly>
+                        {/* <textarea name="content" id="content"></textarea>
+                          <Button
+                            className="bg-blue-700 text-white"
+                            variant="outline"
+                            type="submit"
+                            name="_action"
+                            value="PUBLISH"
+                          >
+                            Submit
+                          </Button>
+                        </Form> */}
+                      </>
+                    )}
+                  </div>
+                </div>
               </li>
-              {p.comment && p.comment.length > 0 && (
-                <Form method="post">
-                  <input
-                    className="hidden"
-                    value={p.id}
-                    name="postId"
-                    id="postId"
-                  />
-                  <Button
-                    variant="ghost"
-                    className="flex gap-2"
-                    type="submit"
-                    name="_action"
-                    value="GET_COMMENTS"
-                    onClick={() => handleReplyClick(p.id)}
-                  >
-                    <ReplyIcon className="h-4 w-4" />
-                    {p.comment.length} Replies
-                  </Button>
-                </Form>
-              )}
 
               {/* {p.comment.length > 0 && <div>{p.comment.length} Replies</div>} */}
-              {showComments[p.id] && (
-                <>
-                  {p.comment &&
-                    p.comment.map((c: Comment) => {
-                      return <span>{c.content}</span>;
-                    })}
-                  <Form method="post" className="w-full">
-                    <input
-                      className="hidden"
-                      id="postId"
-                      name="postId"
-                      value={p.id}
-                    />
-                    <textarea name="content" id="content"></textarea>
-                    <Button
-                      className="bg-blue-700 text-white"
-                      variant="outline"
-                      type="submit"
-                      name="_action"
-                      value="PUBLISH"
-                    >
-                      Submit
-                    </Button>
-                  </Form>
-                </>
-              )}
             </>
           );
         })}
