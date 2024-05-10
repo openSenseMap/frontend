4925053f086c10fa35af72367ab2a071dd334dc5
Matthias Pfeil <matthias.pfeil@wwu.de>
2024-05-10 10:33:48 +0200

    add retention policies

--- drizzle/0002_needy_the_liberteens.sql
+++ drizzle/0002_needy_the_liberteens.sql
@@ -6,6 +6,12 @@ CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 -- Turn the measurement table into a hypertable
 SELECT create_hypertable('measurement', 'time');
 
+-- For setting up downsampling and data retention check the Timescale visualizer and code generator
+-- https://docs.timescale.com/use-timescale/latest/continuous-aggregates/drop-data/#set-up-downsampling-and-data-retention
+
+-- Add retention policy for raw data (Drop raw data older than 1 year)
+SELECT add_retention_policy('measurement', INTERVAL '1 year');
+
 -- Continuous aggregate (CAGG) of the hypertable
 CREATE MATERIALIZED VIEW measurement_15min WITH (timescaledb.continuous) AS
 SELECT measurement."sensorId",
@@ -16,7 +22,12 @@ GROUP BY 1, 2
 WITH NO DATA;
 
 -- Add a CAGG policy in order to refresh it automatically
-SELECT add_continuous_aggregate_policy('measurement_15min', start_offset => INTERVAL '1 day', end_offset => INTERVAL '1 hour', schedule_interval => INTERVAL '15 min');
+-- Refresh: Automatically keep downsampled data up to date with new data from 30 minutes to 1 week ago.
+SELECT add_continuous_aggregate_policy('measurement_15min', start_offset => INTERVAL '1 week', end_offset => INTERVAL '1 hour', schedule_interval => INTERVAL '15 minutes');
+
+-- We do not activate dropping downsampled data for now
+-- Drop downsampled data: Drop downsampled data older than 5 years
+-- SELECT add_retention_policy('measurement_15min', INTERVAL '5 years');
 
 -- Continuous aggregate (CAGG) on top of another CAGG / Hierarchical Continuous Aggregates , new in Timescale 2.9, issue with TZ as of https://github.com/timescale/timescaledb/pull/5195
 CREATE MATERIALIZED VIEW measurement_1day WITH (timescaledb.continuous) AS
@@ -28,5 +39,8 @@ GROUP BY 1, 2
 WITH NO DATA;
 
 -- Add a CAGG policy in order to refresh it automatically
-SELECT add_continuous_aggregate_policy('measurement_1day', start_offset => INTERVAL '3 days', end_offset => INTERVAL '1 day', schedule_interval => INTERVAL '15 min');
+SELECT add_continuous_aggregate_policy('measurement_1day', start_offset => INTERVAL '1 week', end_offset => INTERVAL '2 days', schedule_interval => INTERVAL '1 day');
 
+-- We do not activate dropping downsampled data for now
+-- Drop downsampled data: Drop downsampled data older than 5 years
+-- SELECT add_retention_policy('measurement_1day', INTERVAL '5 years');
