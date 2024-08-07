7cc21baf69b19798f5533bf6d899f7cc179e370b
freds-dev <f_bruc03@uni-muenster.de>
2024-08-07 09:37:26 +0200

    change breakpoint for cards in device detail box

--- app/components/device-detail/device-detail-box.tsx
+++ app/components/device-detail/device-detail-box.tsx
@@ -381,7 +381,7 @@ export default function DeviceDetailBox() {
                             : ""
                         }
                       >
-                        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                        <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-4">
                           {sensors
                             ? sensors.map((sensor: SensorWithMeasurement) => {
                                 return (
