43f79d8bdaae9e8bf02c70b22a4a17defbb94d94
Matthias Pfeil <matthias.pfeil@wwu.de>
2024-10-11 11:50:44 +0200

    drop last migration

--- drizzle/0008_add_location_to_measurement.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "measurement" ADD COLUMN "location" geometry(point);--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "measurement_location_index" ON "measurement" USING gist ("location");
