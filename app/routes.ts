cb0e8d523b9676ef8cdb34bb81eb35564034a5d0
freds-dev <f_bruc03@uni-muenster.de>
2025-01-06 14:21:52 +0100

    refactor: add return

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,6 @@ import { type RouteConfig } from "@react-router/dev/routes";
 import { remixRoutesOptionAdapter } from "@react-router/remix-routes-option-adapter";
 import { flatRoutes } from "remix-flat-routes";
 
-export default remixRoutesOptionAdapter((defineRoutes) =>
-  flatRoutes("routes", defineRoutes)
-) satisfies RouteConfig;
+export default remixRoutesOptionAdapter((defineRoutes) => {
+  return flatRoutes("routes", defineRoutes);
+}) satisfies RouteConfig;
