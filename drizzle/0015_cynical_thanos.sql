8e5b6079a54694264111c16f92f1f76012d9431a
freds-dev <frebruc@gmail.com>
2025-01-28 14:55:11 +0100

    use bigserial and bigint with foreign key constraints

--- drizzle/0015_cynical_thanos.sql
+++ drizzle/0015_cynical_thanos.sql
@@ -5,15 +5,15 @@ ALTER TABLE "measurement" DROP CONSTRAINT "measurement_location_id_location_id_f
 -- Add temporary bigserial/bigint columns
 ALTER TABLE "device_to_location" ADD COLUMN "location_id_bigint" bigint;--> statement-breakpoint
 ALTER TABLE "measurement" ADD COLUMN "location_id_bigint" bigint;--> statement-breakpoint
-ALTER TABLE "location" ADD COLUMN "id_bigserial" bigserial;
+ALTER TABLE "location" ADD COLUMN "id_bigserial" bigserial; --> statement-breakpoint
 
 -- Copy old id values
-UPDATE location SET id_bigserial = id;
-UPDATE device_to_location SET location_id_bigint = location_id;
-UPDATE measurement SET location_id_bigint = location_id;
+UPDATE location SET id_bigserial = id; --> statement-breakpoint
+UPDATE device_to_location SET location_id_bigint = location_id; --> statement-breakpoint
+UPDATE measurement SET location_id_bigint = location_id; --> statement-breakpoint
 
 -- Update last_value of sequence
-SELECT setval('location_id_bigserial_seq', COALESCE(MAX(id), 1)) FROM location;
+SELECT setval('location_id_bigserial_seq', COALESCE(MAX(id), 1)) FROM location; --> statement-breakpoint
 
 -- Drop old id column in location table
 ALTER TABLE "location" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
@@ -32,3 +32,9 @@ ALTER TABLE "device_to_location" ADD CONSTRAINT "device_to_location_device_id_lo
 -- (measurement) Drop old location_id and rename bigint location id
 ALTER TABLE "measurement" DROP COLUMN IF EXISTS "location_id";--> statement-breakpoint
 ALTER TABLE "measurement" RENAME COLUMN "location_id_bigint" TO "location_id";
+
+-- Add foreign key constraint for device_to_location.location_id
+ALTER TABLE "device_to_location" ADD CONSTRAINT "device_to_location_location_id_fkey" FOREIGN KEY ("location_id") REFERENCES "location" ("id"); --> statement-breakpoint
+
+-- Add foreign key constraint for measurement.location_id
+ALTER TABLE "measurement"  ADD CONSTRAINT "measurement_location_id_fkey" FOREIGN KEY ("location_id") REFERENCES "location" ("id");
