353965f9408595aa04c451f2b33c986d3123f6f7
Matthias Pfeil <matthias.pfeil@wwu.de>
2025-01-28 12:54:35 +0100

    change to bigserial

--- app/schema/measurement.ts
+++ app/schema/measurement.ts
@@ -1,93 +1,96 @@
-import { relations, type InferSelectModel } from "drizzle-orm";
+import { relations, type InferSelectModel } from 'drizzle-orm'
 import {
-  doublePrecision,
-  integer,
-  pgMaterializedView,
-  pgTable,
-  text,
-  timestamp,
-  unique,
-} from "drizzle-orm/pg-core";
-import { location } from "./location";
+	bigint,
+	doublePrecision,
+	integer,
+	pgMaterializedView,
+	pgTable,
+	text,
+	timestamp,
+	unique,
+} from 'drizzle-orm/pg-core'
+import { location } from './location'
 
 /**
  * Table
  */
 export const measurement = pgTable(
-  "measurement",
-  {
-    sensorId: text("sensor_id").notNull(),
-    time: timestamp("time", { precision: 3, withTimezone: true })
-      .defaultNow()
-      .notNull(),
-    value: doublePrecision("value"),
-    locationId: integer("location_id").references(() => location.id),
-  },
-  (t) => ({
-    unq: unique().on(t.sensorId, t.time), // Only one measurement for a sensor at the same time
-  }),
-);
+	'measurement',
+	{
+		sensorId: text('sensor_id').notNull(),
+		time: timestamp('time', { precision: 3, withTimezone: true })
+			.defaultNow()
+			.notNull(),
+		value: doublePrecision('value'),
+		locationId: bigint('location_id', { mode: 'bigint' }).references(
+			() => location.id,
+		),
+	},
+	(t) => ({
+		unq: unique().on(t.sensorId, t.time), // Only one measurement for a sensor at the same time
+	}),
+)
 
 /**
  * Relations
  * 1. One-to-many: One measurement could have exactly on location (mobile) or no location (stationary)
  */
 export const measurementRelations = relations(measurement, ({ one }) => ({
-  location: one(location, {
-    fields: [measurement.locationId],
-    references: [location.id],
-  }),
-}));
+	location: one(location, {
+		fields: [measurement.locationId],
+		references: [location.id],
+	}),
+}))
 
 /**
  * Views
  */
-export const measurements10minView = pgMaterializedView("measurement_10min", {
-  sensorId: text("sensor_id"),
-  time: timestamp("time", { precision: 3, withTimezone: true }),
-  value: doublePrecision("avg_value"),
-  total_values: integer("total_values"),
-  min_value: doublePrecision("min_value"),
-  max_value: doublePrecision("max_value"),
-}).existing();
+export const measurements10minView = pgMaterializedView('measurement_10min', {
+	sensorId: text('sensor_id'),
+	time: timestamp('time', { precision: 3, withTimezone: true }),
+	value: doublePrecision('avg_value'),
+	total_values: integer('total_values'),
+	min_value: doublePrecision('min_value'),
+	max_value: doublePrecision('max_value'),
+}).existing()
 
-export const measurements1hourView = pgMaterializedView("measurement_1hour", {
-  sensorId: text("sensor_id"),
-  time: timestamp("time", { precision: 3, withTimezone: true }),
-  value: doublePrecision("avg_value"),
-  total_values: integer("total_values"),
-  min_value: doublePrecision("min_value"),
-  max_value: doublePrecision("max_value"),
-}).existing();
+export const measurements1hourView = pgMaterializedView('measurement_1hour', {
+	sensorId: text('sensor_id'),
+	time: timestamp('time', { precision: 3, withTimezone: true }),
+	value: doublePrecision('avg_value'),
+	total_values: integer('total_values'),
+	min_value: doublePrecision('min_value'),
+	max_value: doublePrecision('max_value'),
+}).existing()
 
-export const measurements1dayView = pgMaterializedView("measurement_1day", {
-  sensorId: text("sensor_id"),
-  time: timestamp("time", { precision: 3, withTimezone: true }),
-  value: doublePrecision("avg_value"),
-  total_values: integer("total_values"),
-  min_value: doublePrecision("min_value"),
-  max_value: doublePrecision("max_value"),
-}).existing();
+export const measurements1dayView = pgMaterializedView('measurement_1day', {
+	sensorId: text('sensor_id'),
+	time: timestamp('time', { precision: 3, withTimezone: true }),
+	value: doublePrecision('avg_value'),
+	total_values: integer('total_values'),
+	min_value: doublePrecision('min_value'),
+	max_value: doublePrecision('max_value'),
+}).existing()
 
-export const measurements1monthView = pgMaterializedView("measurement_1month", {
-  sensorId: text("sensor_id"),
-  time: timestamp("time", { precision: 3, withTimezone: true }),
-  value: doublePrecision("avg_value"),
-  total_values: integer("total_values"),
-  min_value: doublePrecision("min_value"),
-  max_value: doublePrecision("max_value"),
-}).existing();
+export const measurements1monthView = pgMaterializedView('measurement_1month', {
+	sensorId: text('sensor_id'),
+	time: timestamp('time', { precision: 3, withTimezone: true }),
+	value: doublePrecision('avg_value'),
+	total_values: integer('total_values'),
+	min_value: doublePrecision('min_value'),
+	max_value: doublePrecision('max_value'),
+}).existing()
 
-export const measurements1yearView = pgMaterializedView("measurement_1year", {
-  sensorId: text("sensor_id"),
-  time: timestamp("time", { precision: 3, withTimezone: true }),
-  value: doublePrecision("avg_value"),
-  total_values: integer("total_values"),
-  min_value: doublePrecision("min_value"),
-  max_value: doublePrecision("max_value"),
-}).existing();
+export const measurements1yearView = pgMaterializedView('measurement_1year', {
+	sensorId: text('sensor_id'),
+	time: timestamp('time', { precision: 3, withTimezone: true }),
+	value: doublePrecision('avg_value'),
+	total_values: integer('total_values'),
+	min_value: doublePrecision('min_value'),
+	max_value: doublePrecision('max_value'),
+}).existing()
 
 /**
  * Types
  */
-export type Measurement = InferSelectModel<typeof measurement>;
+export type Measurement = InferSelectModel<typeof measurement>
---
+++ drizzle/0015_cynical_thanos.sql
@@ -0,0 +1,34 @@
+-- DROP CONSTRAINTS
+ALTER TABLE "device_to_location" DROP CONSTRAINT "device_to_location_location_id_location_id_fk";--> statement-breakpoint
+ALTER TABLE "measurement" DROP CONSTRAINT "measurement_location_id_location_id_fk";--> statement-breakpoint
+
+-- Add temporary bigserial/bigint columns
+ALTER TABLE "device_to_location" ADD COLUMN "location_id_bigint" bigint;--> statement-breakpoint
+ALTER TABLE "measurement" ADD COLUMN "location_id_bigint" bigint;--> statement-breakpoint
+ALTER TABLE "location" ADD COLUMN "id_bigserial" bigserial;
+
+-- Copy old id values
+UPDATE location SET id_bigserial = id;
+UPDATE device_to_location SET location_id_bigint = location_id;
+UPDATE measurement SET location_id_bigint = location_id;
+
+-- Update last_value of sequence
+SELECT setval('location_id_bigserial_seq', COALESCE(MAX(id), 1)) FROM location;
+
+-- Drop old id column in location table
+ALTER TABLE "location" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
+ALTER TABLE "location" ADD PRIMARY KEY ("id_bigserial");--> statement-breakpoint
+ALTER TABLE "location" ALTER COLUMN "id_bigserial" SET NOT NULL;--> statement-breakpoint
+
+-- Rename temporary bigserial id to id
+ALTER TABLE "location" RENAME COLUMN "id_bigserial" TO "id";--> statement-breakpoint
+
+-- (device_to_location) Drop old location_id and rename bigint location id
+ALTER TABLE "device_to_location" DROP CONSTRAINT "device_to_location_device_id_location_id_time_pk";--> statement-breakpoint
+ALTER TABLE "device_to_location" DROP COLUMN IF EXISTS "location_id";--> statement-breakpoint
+ALTER TABLE "device_to_location" RENAME COLUMN "location_id_bigint" TO "location_id";--> statement-breakpoint
+ALTER TABLE "device_to_location" ADD CONSTRAINT "device_to_location_device_id_location_id_time_pk" PRIMARY KEY("device_id","location_id","time");--> statement-breakpoint
+
+-- (measurement) Drop old location_id and rename bigint location id
+ALTER TABLE "measurement" DROP COLUMN IF EXISTS "location_id";--> statement-breakpoint
+ALTER TABLE "measurement" RENAME COLUMN "location_id_bigint" TO "location_id";
