ec53fc88d0398a7cf3052de0edcb0a2c7a48c2a7
David Scheidt <david.scheidt@opensenselab.org>
2025-04-16 13:45:14 +0200

    feat: migrate to latest directus sdk

--- app/lib/directus.ts
+++ app/lib/directus.ts
@@ -1,40 +1,45 @@
-import { Directus, type ID  } from '@directus/sdk';
+import {
+  createDirectus,
+  type DirectusClient,
+  rest,
+  type RestClient,
+} from "@directus/sdk";
 
-const directusUrl = process.env.DIRECTUS_URL || 'http://localhost:8055'
+const directusUrl = process.env.DIRECTUS_URL || "http://localhost:8055";
 
 export type UseCase = {
-    id: ID,
-    status: string,
-    image: string,
-    title: string,
-    description: string,
-    content: string,
-    language: "de" | "en"
-}
+  id: number | string;
+  status: string;
+  image: string;
+  title: string;
+  description: string;
+  content: string;
+  language: "de" | "en";
+};
 
 export type Feature = {
-    id: ID,
-    title: string,
-    description: string,
-    icon: string,
-    language: "de" | "en"
-}
+  id: number | string;
+  title: string;
+  description: string;
+  icon: string;
+  language: "de" | "en";
+};
 
 export type Partner = {
-    id: ID,
-    name: string,
-    logo: string,
-    link: string
-}
+  id: number | string;
+  name: string;
+  logo: string;
+  link: string;
+};
 
 type DirectusCollection = {
-    use_cases: UseCase,
-    features: Feature,
-    partners: Partner
-}
+  use_cases: UseCase[];
+  features: Feature[];
+  partners: Partner[];
+};
 
-const directus = new Directus<DirectusCollection>(directusUrl)
+const directus = createDirectus<DirectusCollection>(directusUrl).with(rest());
 
-export async function getDirectusClient () {
-    return directus
+export function getDirectusClient(): DirectusClient<DirectusCollection> & RestClient<DirectusCollection> {
+  return directus;
 }
