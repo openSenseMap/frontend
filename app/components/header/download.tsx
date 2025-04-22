f5d3bf7b16af9381dda51893800a79fff0a13fb4
JerryVincent <jerrynambalampara111@gmail.com>
2025-04-22 14:42:05 +0200

    removed unused code

--- app/components/header/download.tsx
+++ app/components/header/download.tsx
@@ -336,21 +336,6 @@ const handleDownloadStart = () => {
                      {downloadStarted ? "Downloading..." : `Download ${format.toUpperCase()} Data`}
                       </a>
                           ) : null}
-                
-                {/* {actionData && isDownloadReady ? (
-                  <a 
-                    href={actionData.href} 
-                    download={actionData.download}
-                    className="px-4 py-2 bg-green-100 text-dark rounded hover:bg-green-400 transition-colors flex items-center"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2">
-                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                      <polyline points="7 10 12 15 17 10"></polyline>
-                      <line x1="12" y1="15" x2="12" y2="3"></line>
-                    </svg>
-                    Download {format.toUpperCase()} Data
-                  </a>
-                ) : null} */}
               </div>
             </DialogFooter>
           </Form>
