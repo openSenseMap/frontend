ad6862fea97db88b50430069710f6277528c5244
David Scheidt <david.scheidt@opensenselab.org>
2025-09-17 16:19:46 +0200

    feat: add nodemailer and setup for sending mails

---
+++ app/lib/mail.server.ts
@@ -0,0 +1,95 @@
+import { render } from "@react-email/components";
+import * as dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
+import { type JSX } from "react";
+dotenv.config();
+
+// Interface to load env variables
+// Note these variables can possibly be undefined
+// as someone could skip these varibales or not setup a .env file at all
+
+/**
+ * Interface for our configuration.
+ * Note these variables can possibly be undefined,
+ * as varibales may be skipped or  there is no .env file at all
+ */
+interface Config {
+  SMTP_HOST: string | undefined;
+  SMTP_PORT: number | undefined;
+  SMTP_SECURE: boolean | undefined;
+  SMTP_USERNAME: string | undefined;
+  SMTP_PASSWORD: string | undefined;
+}
+
+/**
+ * Processes environment variables and
+ * builds a configuration object from it.
+ * @returns {Config} A mailer configuration
+ */
+const getConfig = (): Config => {
+  const config = {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT
+      ? Number(process.env.SMTP_PORT)
+      : undefined,
+    SMTP_SECURE: process.env.SMTP_SECURE
+      ? Boolean(JSON.parse(process.env.SMTP_SECURE))
+      : undefined,
+    SMTP_USERNAME: process.env.SMTP_USERNAME,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  };
+
+  // check the config for missing entries and throw an error if necessary
+  for (const [key, value] of Object.entries(config)) {
+    if (value === undefined) {
+      throw new Error(`Missing key ${key} in config.env`);
+    }
+  }
+
+  return config;
+};
+
+const config = getConfig();
+const transportOptions: SMTPTransport.Options = {
+  host: config.SMTP_HOST,
+  port: config.SMTP_PORT,
+  secure: config.SMTP_SECURE,
+};
+if (config.SMTP_USERNAME !== "ignored" && config.SMTP_PASSWORD !== "ignored") {
+  transportOptions["auth"] = {
+    user: config.SMTP_USERNAME,
+    pass: config.SMTP_PASSWORD,
+  };
+}
+const transporter = nodemailer.createTransport(transportOptions);
+
+export interface MailAttachment {
+  filename: string;
+  content: string;
+}
+
+export const sendMail = async (mailConfig: {
+  recipientAddress: string;
+  recipientName?: string;
+  subject?: string;
+  body: React.ReactElement;
+  attachments?: MailAttachment[];
+}) => {
+  try {
+    const mailHtml = await render(mailConfig.body);
+
+    await transporter.sendMail({
+      from: '"openSenseMap 🌍" <no-reply@opensensemap.org>',
+      to: mailConfig.recipientName
+        ? `"${mailConfig.recipientName}" <${mailConfig.recipientAddress}>`
+        : mailConfig.recipientAddress,
+      subject: mailConfig.subject ?? "openSenseMap",
+      html: mailHtml,
+      attachments: mailConfig.attachments,
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
