6968495ddb054cfb564fd1300311514a2f691049
jona159 <raabejonas@yahoo.de>
2025-09-25 10:49:35 +0200

    fix: migrations

---
+++ drizzle/0021_bouncy_terror.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS "claim" (
+	"id" text PRIMARY KEY NOT NULL,
+	"box_id" text NOT NULL,
+	"token" text,
+	"expires_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "unique_box_id" UNIQUE("box_id")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "claim" ADD CONSTRAINT "claim_box_id_device_id_fk" FOREIGN KEY ("box_id") REFERENCES "public"."device"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "claim_expires_at_idx" ON "claim" USING btree ("expires_at");
