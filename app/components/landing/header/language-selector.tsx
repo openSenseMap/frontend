c606e5c1d71c9f4b284bc6918b391f46e4a3029a
David Scheidt <david.scheidt@opensenselab.org>
2025-11-26 12:23:06 +0100

    refactor: remove unused state from selector

--- app/components/landing/header/language-selector.tsx
+++ app/components/landing/header/language-selector.tsx
@@ -1,44 +1,41 @@
-import i18next from "i18next";
-import { Globe } from "lucide-react";
-import { useEffect, useState } from "react";
-import { useFetcher, useLoaderData } from "react-router";
-import { Button } from "~/components/ui/button";
-// import  { type loader } from "~/root";
+import i18next from 'i18next'
+import { Globe } from 'lucide-react'
+import { useEffect } from 'react'
+import { useFetcher, useLoaderData } from 'react-router'
+import { Button } from '~/components/ui/button'
+import { type loader } from '~/root'
 
 export default function LanguageSelector() {
-  const data = useLoaderData();
-  const fetcher = useFetcher();
-  const [locale, setLocale] = useState(data.locale || "en");
-  // When loader locale changes (e.g. after login), sync state
-  useEffect(() => {
-    if (!data?.locale) return;
-    setLocale(data.locale);
-    void (async () => {
-      try {
-        await i18next.changeLanguage(data.locale);
-      } catch (e) {
-        //  Promises must be awaited, end with a call to .catch, end with a call to .then with a rejection handler
-      }
-    })();
-  }, [data.locale]);
-  const toggleLanguage = () => {
-    const newLocale = locale === "en" ? "de" : "en"; // Toggle between "en" and "de"
-    setLocale(newLocale);
-    void i18next.changeLanguage(newLocale); // Change the language in the app
-    void fetcher.submit(
-      { language: newLocale },
-      { method: "post", action: "/action/set-language" }, // Persist the new language
-    );
-  };
+	const data = useLoaderData<typeof loader>()
+	const fetcher = useFetcher()
 
-  return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={toggleLanguage}
-      className="hover:bg-transparent dark:hover:text-white hover:text-black"
-    >
-      <Globe/>{locale === "de" ? <p>DE</p> : <p>EN</p>}
-    </Button>
-  );
+	// When loader locale changes (e.g. after login), sync state
+	useEffect(() => {
+		if (!data?.locale) return
+		const updateLang = async () => {
+			await i18next.changeLanguage(data.locale)
+		}
+		void updateLang()
+	}, [data.locale])
+
+	const toggleLanguage = async () => {
+		const newLocale = (data?.locale ?? 'en') === 'en' ? 'de' : 'en' // Toggle between "en" and "de"
+		void fetcher.submit(
+			{ language: newLocale },
+			{ method: 'post', action: '/action/set-language' }, // Persist the new language
+		)
+		await i18next.changeLanguage(newLocale) // Change the language in the app
+	}
+
+	return (
+		<Button
+			variant="ghost"
+			size="icon"
+			onClick={toggleLanguage}
+			className="hover:bg-transparent hover:text-black dark:hover:text-white"
+		>
+			<Globe />
+			{(data?.locale ?? 'en') === 'de' ? <p>DE</p> : <p>EN</p>}
+		</Button>
+	)
 }
