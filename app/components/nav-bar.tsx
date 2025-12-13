58e9ddb78ec6e6d7192c28501836ca5d8d318f50
Timber <timber@opensenselab.org>
2025-12-13 16:53:40 +0100

    feat: link to explore page in profile / settings instead of root

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -30,7 +30,7 @@ export function NavBar() {
 		<div className="border-b bg-white p-4 dark:bg-dark-background dark:text-dark-text">
 			<div className="flex h-16 items-center justify-between">
 				<div className="flex max-w-screen-xl flex-wrap items-center justify-between">
-					<Link to="/" className="flex items-center md:pr-4">
+					<Link to="/explore" className="flex items-center md:pr-4">
 						<img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="osem Logo" />
 					</Link>
 					<span className="hidden self-center whitespace-nowrap text-xl font-semibold text-light-green dark:text-dark-green md:block">
