24fd9c7476d76c4f20e5e9843ce592049ab09655
jona159 <raabejonas@yahoo.de>
2026-01-09 15:47:24 +0100

    feat: add mqtt service url and key to env

--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -17,6 +17,8 @@ const schema = z.object({
   MYBADGES_ISSUERID_OSEM: z.string(),
   MYBADGES_CLIENT_ID: z.string(),
   MYBADGES_CLIENT_SECRET: z.string(),
+  MQTT_SERVICE_URL: z.string(),
+  MQTT_SERVICE_KEY: z.string()
 });
 
 declare global {
@@ -45,6 +47,7 @@ export function getEnv() {
     MYBADGES_API_URL: process.env.MYBADGES_API_URL,
     MYBADGES_URL: process.env.MYBADGES_URL,
     SENSORWIKI_API_URL: process.env.SENSORWIKI_API_URL,
+    MQTT_SERVICE_URL: process.env.MQTT_SERVICE_URL,
   };
 }
 
