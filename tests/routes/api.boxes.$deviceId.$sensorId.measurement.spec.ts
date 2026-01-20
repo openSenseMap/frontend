1fea522bb4de1cd3dab61e173f56bc73fca66c33
David Scheidt <david.scheidt@opensenselab.org>
2026-01-20 13:36:28 +0100

    test: add delete test for foreign devices

--- tests/routes/api.boxes.$deviceId.$sensorId.measurement.spec.ts
+++ tests/routes/api.boxes.$deviceId.$sensorId.measurement.spec.ts
@@ -8,9 +8,10 @@ import { insertMeasurements } from '~/models/measurement.server'
 import { getSensors } from '~/models/sensor.server'
 import { deleteUserByEmail } from '~/models/user.server'
 import { action } from '~/routes/api.boxes.$deviceId.$sensorId.measurements'
-import { type User } from '~/schema'
+import { sensor, type User } from '~/schema'
 
 const USER = generateTestUserCredentials()
+const USER2 = generateTestUserCredentials()
 const DEVICE = {
 	name: `${USER.name}s Box`,
 	exposure: 'outdoor',
@@ -52,9 +53,16 @@ describe('openSenseMap API Routes: /boxes/:deviceId/:sensorId/measurement', () =
 	let deviceId: string
 	let sensorId: string
 	let jwt: string
+	let jwt2: string
 
 	beforeAll(async () => {
 		const u = await registerUser(USER.name, USER.email, USER.password, 'en_US')
+		const u2 = await registerUser(
+			USER2.name,
+			USER2.email,
+			USER2.password,
+			'en_US',
+		)
 		const d = await createDevice(DEVICE, (u as User).id)
 		const s = await getSensors(d.id)
 		MEASUREMENTS.forEach((m) => (m.sensor_id = s[0].id))
@@ -65,6 +73,9 @@ describe('openSenseMap API Routes: /boxes/:deviceId/:sensorId/measurement', () =
 
 		const t = await createToken(u as User)
 		jwt = t.token
+
+		const t2 = await createToken(u2 as User)
+		jwt2 = t2.token
 	})
 
 	describe('DELETE', () => {
@@ -139,10 +150,37 @@ describe('openSenseMap API Routes: /boxes/:deviceId/:sensorId/measurement', () =
 			expect(body).toHaveProperty('message')
 			expect(body.message).toBe(`Successfully deleted 1 of sensor ${sensorId}`)
 		})
+
+		it('should deny to delete sensordata of boxes not owned by the user', async () => {
+			// Arrange
+			const request = new Request(
+				`${BASE_URL}/api/boxes/${deviceId}/${sensorId}?deleteAllMeasurements=true`,
+				{ method: 'DELETE', headers: { Authorization: `Bearer ${jwt2}` } },
+			)
+
+			// Act
+			const dataFunctionValue = await action({
+				request,
+				params: {
+					deviceId: `${deviceId}`,
+					sensorId: `${sensorId}`,
+				} as Params<string>,
+			} as ActionFunctionArgs)
+			const response = dataFunctionValue as Response
+			const body = await response?.json()
+
+			// Assert
+			expect(response.status).toBe(403)
+			expect(body).toHaveProperty('error')
+			expect(body.error).toBe(
+				'You are not allowed to delete data of the given device',
+			)
+		})
 	})
 
 	afterAll(async () => {
 		await deleteUserByEmail(USER.email)
+		await deleteUserByEmail(USER2.email)
 		await deleteDevice({ id: deviceId })
 	})
 })
