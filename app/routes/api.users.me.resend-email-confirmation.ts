0c399800fc8147b641a7db563f665a9f1e35d7c8
JerryVincent <jerrynambalampara111@gmail.com>
2026-02-04 15:29:11 +0100

    This commit adds null check before passing jwtresponse to resendEmailConfirmation function.

--- app/routes/api.users.me.resend-email-confirmation.ts
+++ app/routes/api.users.me.resend-email-confirmation.ts
@@ -1,28 +1,30 @@
-import { type ActionFunction, type ActionFunctionArgs } from "react-router";
-import { resendEmailConfirmation } from "~/lib/user-service.server";
-import { type User } from "~/schema";
-import { StandardResponse } from "~/utils/response-utils";
-import { getUser } from "~/utils/session.server";
+import { type ActionFunction, type ActionFunctionArgs } from 'react-router'
+import { resendEmailConfirmation } from '~/lib/user-service.server'
+import { StandardResponse } from '~/utils/response-utils'
+import { getUser } from '~/utils/session.server'
 
 export const action: ActionFunction = async ({
-  request,
+	request,
 }: ActionFunctionArgs) => {
-  try {
-    const jwtResponse = await getUser(request) as User;
-    
-    if (typeof jwtResponse === "string")
-      return StandardResponse.forbidden("Invalid JWT authorization. Please sign in to obtain new JWT.");
+	try {
+		const jwtResponse = await getUser(request)
+		if (!jwtResponse || typeof jwtResponse === 'string')
+			return StandardResponse.forbidden(
+				'Invalid JWT authorization. Please sign in to obtain new JWT.',
+			)
 
-    const result = await resendEmailConfirmation(jwtResponse);
-    if (result === "already_confirmed")
-      return StandardResponse.unprocessableContent(`Email address ${jwtResponse.email} is already confirmed.`);
+		const result = await resendEmailConfirmation(jwtResponse)
+		if (result === 'already_confirmed')
+			return StandardResponse.unprocessableContent(
+				`Email address ${jwtResponse.email} is already confirmed.`,
+			)
 
-    return StandardResponse.ok({
-        code: "Ok",
-        message: `Email confirmation has been sent to ${result.unconfirmedEmail}`,
-      });
-  } catch (err) {
-    console.warn(err);
-    return StandardResponse.internalServerError();
-  }
-};
+		return StandardResponse.ok({
+			code: 'Ok',
+			message: `Email confirmation has been sent to ${result.unconfirmedEmail}`,
+		})
+	} catch (err) {
+		console.warn(err)
+		return StandardResponse.internalServerError()
+	}
+}
