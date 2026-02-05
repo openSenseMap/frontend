19c9ed6bf890797d5f772334c057a1bc12d2a635
David Scheidt <david.scheidt@opensenselab.org>
2026-02-05 10:23:29 +0100

    feat: sketch post and put for generating new token and updating status

--- app/routes/device.$deviceId.edit.security.tsx
+++ app/routes/device.$deviceId.edit.security.tsx
@@ -13,6 +13,7 @@ import {
 	redirect,
 	Form,
 	useLoaderData,
+	type ActionFunctionArgs,
 } from 'react-router'
 import { Checkbox } from '@/components/ui/checkbox'
 import ErrorMessage from '~/components/error-message'
@@ -33,8 +34,17 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 	return { key: t?.token, deviceAuthEnabled: device?.useAuth ?? false }
 }
 
-export async function action() {
-	console.log('hello world')
+export async function action({ request }: ActionFunctionArgs) {
+	switch (request.method) {
+		case 'POST':
+			break
+		case 'PUT':
+			break
+	}
+
+	const formData = await request.formData()
+	console.log(formData)
+
 	return ''
 }
 
@@ -49,107 +59,109 @@ export default function EditBoxSecurity() {
 	}
 
 	return (
-		<Form method="post" className="font-helvetica text-[14px]" noValidate>
-			<div className="mt-2 flex justify-between">
-				<h1 className="text-4xl">{t('device_security.page_title')}</h1>
-				<button
-					name="intent"
-					value="save"
-					className="h-12 w-12 rounded-full border-[1.5px] border-[#9b9494] hover:bg-[#e7e6e6]"
-				>
-					<Save className="mx-auto h-5 w-5 lg:h-7 lg:w-7" />
-				</button>
-			</div>
-
-			<hr className="my-3 mt-6 h-px border-0 bg-[#dcdada] dark:bg-gray-700" />
-
-			<p className="py-4">
-				<Trans t={t} i18nKey="device_security.explanation_text">
-					Devices should use their API key shown on this page to authenticate
-					requests sent to the openSenseMap API. This ensures that only
-					authenticated devices update the state of the device on openSenseMap.
-					The API key is appended to every request made to the API. More
-					information can be found{' '}
-					<a
-						href="https://docs.opensensemap.org/#api-Measurements-postNewMeasurement"
-						className="underline"
-					>
-						in the docs
-					</a>
-					.
-				</Trans>
-			</p>
-
-			<Callout variant="caution">
-				{t('device_security.warning_deactive_auth')}
-			</Callout>
-
-			<div className="flex flex-wrap items-center gap-4 py-5">
-				<Checkbox
-					id="enableAuth"
-					value={authEnabled}
-					onChange={() => setAuthEnabled(!authEnabled)}
-				/>
-				<Label htmlFor="enableAuth" className="cursor-pointer pt-1">
-					{t('device_security.auth_enable_checkbox_label')}
-				</Label>
-			</div>
-
-			<div>
-				<Label htmlFor="api-key" className="cursor-pointer">
-					{t('device_security.api_key_label')}
-				</Label>
-				<div className="mt-1 flex">
-					<span>
-						<button
-							className="btn btn-default w-12 rounded-br-none rounded-tr-none"
-							onClick={() => setTokenvisibility(!keyVisible)}
-							disabled={!deviceAuthEnabled}
-							type="button"
-						>
-							{keyVisible ? (
-								<LucideEyeOff size={20.5} />
-							) : (
-								<LucideEye size={20.5} />
-							)}
-						</button>
-					</span>
-					<input
-						name="api-key"
-						id="api-key"
-						value={key}
-						className="form-control rounded-none border-[#ccc;]"
-						type={keyVisible ? 'text' : 'password'}
-						disabled
-					/>
-					<span>
-						<button
-							className="btn btn-default w-12 rounded-bl-none rounded-tl-none"
-							onClick={() => copyKeyToClipboard()}
-							type="button"
-							disabled={!deviceAuthEnabled}
-						>
-							<LucideCopy size={20.5} />
-						</button>
-					</span>
+		<div className="font-helvetica text-[14px]">
+			<Form method="POST" noValidate>
+				<div className="mt-2 flex justify-between">
+					<h1 className="text-4xl">{t('device_security.page_title')}</h1>
+					<button className="h-12 w-12 rounded-full border-[1.5px] border-[#9b9494] hover:bg-[#e7e6e6]">
+						<Save className="mx-auto h-5 w-5 lg:h-7 lg:w-7" />
+					</button>
 				</div>
-			</div>
-			<br />
 
-			<Callout variant="warning">
-				<p>
-					<Trans t={t} i18nKey="device_security.generate_new_key_warning">
-						Generating a new key will require you to update your device (e.g.
-						change the sketch/ code).
-						<b>This step can not be undone.</b>
+				<hr className="my-3 mt-6 h-px border-0 bg-[#dcdada] dark:bg-gray-700" />
+
+				<p className="py-4">
+					<Trans t={t} i18nKey="device_security.explanation_text">
+						Devices should use their API key shown on this page to authenticate
+						requests sent to the openSenseMap API. This ensures that only
+						authenticated devices update the state of the device on
+						openSenseMap. The API key is appended to every request made to the
+						API. More information can be found{' '}
+						<a
+							href="https://docs.opensensemap.org/#api-Measurements-postNewMeasurement"
+							className="underline"
+						>
+							in the docs
+						</a>
+						.
 					</Trans>
 				</p>
-				<button className="btn flex items-center space-x-2 bg-[#e9e9ed]">
-					<RefreshCw className="mr-2 inline h-4 w-4 align-sub" />
-					{t('device_security.generate_new_key_button')}
-				</button>
-			</Callout>
-		</Form>
+
+				<Callout variant="caution">
+					{t('device_security.warning_deactive_auth')}
+				</Callout>
+
+				<div className="flex flex-wrap items-center gap-4 py-5">
+					<Checkbox
+						name="enableAuthCheckbox"
+						id="enableAuthCheckbox"
+						value={authEnabled ? 'on' : 'off'}
+						onChange={() => setAuthEnabled(!authEnabled)}
+					/>
+					<Label htmlFor="enableAuthCheckbox" className="cursor-pointer pt-1">
+						{t('device_security.auth_enable_checkbox_label')}
+					</Label>
+				</div>
+
+				<div>
+					<Label htmlFor="api-key" className="cursor-pointer">
+						{t('device_security.api_key_label')}
+					</Label>
+					<div className="mt-1 flex">
+						<span>
+							<button
+								className="btn btn-default w-12 rounded-br-none rounded-tr-none disabled:opacity-40"
+								onClick={() => setTokenvisibility(!keyVisible)}
+								disabled={!deviceAuthEnabled}
+								type="button"
+							>
+								{keyVisible ? (
+									<LucideEyeOff size={20.5} />
+								) : (
+									<LucideEye size={20.5} />
+								)}
+							</button>
+						</span>
+						<input
+							name="api-key"
+							value={key}
+							className="form-control rounded-none border-[#ccc;]"
+							type={keyVisible ? 'text' : 'password'}
+							disabled
+						/>
+						<span>
+							<button
+								className="btn btn-default w-12 rounded-bl-none rounded-tl-none disabled:opacity-40"
+								onClick={() => copyKeyToClipboard()}
+								type="button"
+								disabled={!deviceAuthEnabled}
+							>
+								<LucideCopy size={20.5} />
+							</button>
+						</span>
+					</div>
+				</div>
+				<br />
+			</Form>
+			<Form method="PUT">
+				<Callout variant="warning">
+					<p>
+						<Trans t={t} i18nKey="device_security.generate_new_key_warning">
+							Generating a new key will require you to update your device (e.g.
+							change the sketch/ code).
+							<b>This step can not be undone.</b>
+						</Trans>
+					</p>
+					<button
+						className="btn flex items-center space-x-2 bg-[#e9e9ed] disabled:opacity-40"
+						disabled={!deviceAuthEnabled}
+					>
+						<RefreshCw className="mr-2 inline h-4 w-4 align-sub" />
+						{t('device_security.generate_new_key_button')}
+					</button>
+				</Callout>
+			</Form>
+		</div>
 	)
 }
 
