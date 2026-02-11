7d3f4c14d95834aac950959d27b906e6b080b649
David Scheidt <david.scheidt@opensenselab.org>
2026-02-11 15:51:44 +0100

    feat: prevent lingering connections in tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,16 @@
 import '@testing-library/jest-dom'
 import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
+import { pg } from '~/db.server'
 
 export const BASE_URL = 'http://localhost:4200'
 
 afterEach(() => {
 	cleanup()
+})
+
+afterAll(async () => {
+	// prevent lingering connections from
+	// blocking other test suites
+	await pg.end()
 })
