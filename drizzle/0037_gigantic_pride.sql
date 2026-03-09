0869dea3a39c2a3d04d8e65e989706eaeed80088
jona159 <raabejonas@yahoo.de>
2026-03-09 10:07:43 +0100

    fix: migrations

--- drizzle/0037_gigantic_pride.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "tos_acceptance" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "tos_version" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "tos_acceptance" CASCADE;--> statement-breakpoint
-DROP TABLE "tos_version" CASCADE;--> statement-breakpoint
-ALTER TABLE "device" ADD CONSTRAINT "device_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE cascade;
