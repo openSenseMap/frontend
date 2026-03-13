166cd0bf39ede6df70057b055035b9fb2cb17aba
David Scheidt <scheidtdav@users.noreply.github.com>
2026-03-13 16:16:29 +0100

    Use CMS to grab imprint (#817)
    * remove dummy imprint
    
    * add imprint page from cms
    
    * revert package changes
    
    * feat: hard install eslint-plugin-react-hooks for proper handling of useEffectEvent
    
    * reinstall markdown-to-jsx
    
    * replace fetcher with revalidator

--- app/routes/imprint.tsx
+++ app/routes/imprint.tsx
@@ -1,119 +1,175 @@
+import { readItem, readItems } from '@directus/sdk'
+import Markdown, { MarkdownToJSX } from 'markdown-to-jsx/react'
+import { useEffect, useEffectEvent } from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+	Link,
+	useFetcher,
+	useLoaderData,
+	useRevalidator,
+	type LoaderFunctionArgs,
+} from 'react-router'
+import LanguageSelector from '~/components/landing/header/language-selector'
+import i18nextOptions, { type supportedLanguages } from '~/i18next-options'
+import i18next from '~/i18next.server'
+import {
+	getDirectusClient,
+	type StaticPageTranslation,
+	type StaticPage,
+} from '~/lib/directus'
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const locale = (await i18next.getLocale(
+		request,
+	)) as (typeof supportedLanguages)[number]
+	const locales =
+		locale == i18nextOptions.fallbackLng
+			? [locale]
+			: [locale, i18nextOptions.fallbackLng]
+	const directus = getDirectusClient()
+	return directus
+		.request<StaticPage>(readItem('static_pages', 'imprint'))
+		.then((data) => {
+			return directus
+				.request<StaticPageTranslation[]>(
+					readItems('static_pages_translations', {
+						limit: locales.length,
+						filter: {
+							static_pages_slug: {
+								_eq: data.slug,
+							},
+							_or: locales.map((l) => ({
+								static_pages_languages_code: {
+									_istarts_with: l,
+								},
+							})),
+						},
+					}),
+				)
+				.then(
+					(i) =>
+						i.sort((a, b) => {
+							const aIdx = locales.findIndex((l) =>
+								a.static_pages_languages_code.startsWith(l),
+							)
+							const bIdx = locales.findIndex((l) =>
+								b.static_pages_languages_code.startsWith(l),
+							)
+							return aIdx - bIdx
+						})[0],
+				)
+		})
+}
+
 export default function Imprint() {
+	const data = useLoaderData<typeof loader>()
+	const [, i18n] = useTranslation()
+	const revalidator = useRevalidator()
+
+	const reloadOnLanguageChanged = useEffectEvent(async () => {
+		await revalidator.revalidate()
+	})
+
+	useEffect(() => {
+		i18n.on('languageChanged', reloadOnLanguageChanged)
+		return () => i18n.off('languageChanged', reloadOnLanguageChanged)
+	}, [i18n])
+
 	return (
-		<div>
-			<h1>Impressum</h1>
-
-			<p>
-				<strong>Angabe gemäß §5 TMG</strong>
-			</p>
-			<p>
-				openSenseLab gGmbh<br></br>
-				Von-Steuben-Str. 21<br></br>
-				48143 Münster<br></br>
-				Deutschland
-			</p>
-			<p>
-				<strong>E-Mail</strong>:{' '}
-				<a href="mailto:info@opensenselab.org">info@opensenselab.org</a>
-			</p>
-			<p>
-				<strong>Telefon</strong>: 0251 / 98 29 88 67
-			</p>
-			<p>
-				<strong>Handelsregister</strong>: HRB 19449
-			</p>
-			<p>
-				<strong>Umsatzsteuernummer:</strong> DE347523917
-			</p>
-			<p>
-				<strong>Registergericht:</strong> Amtsgericht Münster
-			</p>
-			<p>
-				<strong>Vertreten durch die Geschäftsführer:</strong> Dr. Thomas
-				Bartoschek, Mario Pesch
-			</p>
-
-			<h2>Haftungsausschluss</h2>
-			<p>
-				Inhaltlich verantwortlich gemäß § 55 Abs. 2 RStV ist der jeweilige Autor
-				eines Textes.
-			</p>
-
-			<h2>EU-Streitschlichtung</h2>
-			<p>
-				Die Europäische Kommission stellt eine Plattform zur
-				Online-Streitbeilegung (OS) bereit:
-				<a href="https://ec.europa.eu/consumers/odr/" target="_blank">
-					https://ec.europa.eu/consumers/odr/
-				</a>
-				. Unsere E-Mail-Adresse finden Sie oben im Impressum.
-			</p>
-
-			<h2>Verbraucherstreitbeilegung / Universalschlichtungsstelle</h2>
-			<p>
-				Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
-				vor einer Verbraucherschlichtungsstelle teilzunehmen.
-			</p>
-
-			<h2>Haftung für Inhalte</h2>
-			<p>
-				Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
-				diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
-				bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
-				übermittelte oder gespeicherte fremde Informationen zu überwachen oder
-				nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
-				hinweisen.
-			</p>
-			<p>
-				Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
-				Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
-				Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
-				Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
-				entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
-				entfernen.
-			</p>
-
-			<h2>Haftung für Links</h2>
-			<p>
-				Unser Angebot enthält Links zu externen Websites Dritter, auf deren
-				Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
-				Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
-				Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
-				verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
-				Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte
-				waren zum Zeitpunkt der Verlinkung nicht erkennbar.
-			</p>
-			<p>
-				Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
-				ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
-				Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend
-				entfernen.
-			</p>
-
-			<h2>Urheberrecht</h2>
-			<p>
-				Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
-				Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
-				Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
-				Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
-				jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
-				sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
-			</p>
-			<p>
-				Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
-				werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
-				Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
-				Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
-				entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden
-				wir derartige Inhalte umgehend entfernen.
-			</p>
-
-			<p>
-				Quelle:{' '}
-				<a href="https://www.e-recht24.de" target="_blank">
-					https://www.e-recht24.de
-				</a>
-			</p>
-		</div>
+		<>
+			<header>
+				<nav className="relative mx-auto flex h-16 max-w-7xl justify-between py-6 dark:border-gray-300 dark:bg-black">
+					<div className="container flex flex-wrap items-center justify-between px-4 font-serif">
+						<div className="flex max-w-screen-xl flex-wrap items-center justify-between">
+							<Link to="/" className="flex items-center md:pr-10">
+								<img
+									src="/logo.png"
+									className="mr-3 h-6 sm:h-9"
+									alt="osem Logo"
+								/>
+								<span className="hidden self-center whitespace-nowrap text-xl text-light-green dark:text-dark-green md:block">
+									openSenseMap
+								</span>
+							</Link>
+						</div>
+						<div>
+							<div className="flex items-center justify-center md:order-2">
+								<LanguageSelector />
+							</div>
+						</div>
+					</div>
+				</nav>
+			</header>
+			<main className="mx-auto mt-8 flex max-w-7xl flex-col justify-center px-4 sm:px-6 lg:px-8">
+				<h1 className="my-4 text-4xl">{data.title}</h1>
+				<Markdown
+					options={{ overrides: mdOverrides }}
+					children={data.content}
+				/>
+			</main>
+		</>
 	)
+}
+
+const mdOverrides: MarkdownToJSX.Overrides = {
+	h1: {
+		component: ({ children, className, ...props }) => (
+			<h1
+				className={`${className ? className : ''} mb-2 mt-6 text-3xl font-semibold`}
+				{...props}
+			>
+				{children}
+			</h1>
+		),
+	},
+	h2: {
+		component: ({ children, className, ...props }) => (
+			<h2
+				className={`${className ? className : ''} mb-2 mt-6 text-2xl font-semibold`}
+				{...props}
+			>
+				{children}
+			</h2>
+		),
+	},
+	h3: {
+		component: ({ children, className, ...props }) => (
+			<h3
+				className={`${className ? className : ''} mb-2 mt-6 text-lg font-semibold`}
+				{...props}
+			>
+				{children}
+			</h3>
+		),
+	},
+	p: {
+		component: ({ children, className, ...props }) => (
+			<p
+				className={`${className ? className : ''} mb-2 mt-4 text-base`}
+				{...props}
+			>
+				{children}
+			</p>
+		),
+	},
+	a: {
+		component: ({ children, className, ...props }) => (
+			<a
+				className={`${className ? className : ''} text-green-400 hover:underline`}
+				{...props}
+			>
+				{children}
+			</a>
+		),
+	},
+	ul: {
+		component: ({ children, className, ...props }) => (
+			<ul
+				className={`${className ? className : ''} list-inside list-disc space-y-1`}
+				{...props}
+			>
+				{children}
+			</ul>
+		),
+	},
 }
