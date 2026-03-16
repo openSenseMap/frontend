435ba1fffd8d950b970afda4e1127130af24bf89
jona159 <raabejonas@yahoo.de>
2026-03-16 11:33:03 +0100

    fix: fallback bucket name

--- app/utils/s3.server.ts
+++ app/utils/s3.server.ts
@@ -10,7 +10,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const S3_ENDPOINT = (process.env.S3_ENDPOINT || 'http://localhost:9000').replace(/\/$/, '')
 const S3_REGION = process.env.S3_REGION || 'eu-north-1'
-const BUCKET_NAME = process.env.S3_BUCKET || 'devices'
+const BUCKET_NAME = process.env.S3_BUCKET || 'images'
 
 const s3Client = new S3Client({
   endpoint: S3_ENDPOINT,
@@ -24,7 +24,11 @@ const s3Client = new S3Client({
 
 async function ensureBucketExists() {
   try {
-    await s3Client.send(new HeadBucketCommand({ Bucket: BUCKET_NAME }))
+    await s3Client.send(
+      new HeadBucketCommand({
+        Bucket: BUCKET_NAME,
+      }),
+    )
   } catch (error: any) {
     const code = error?.name || error?.Code
     if (
@@ -32,9 +36,14 @@ async function ensureBucketExists() {
       code === 'NoSuchBucket' ||
       error?.$metadata?.httpStatusCode === 404
     ) {
-      await s3Client.send(new CreateBucketCommand({ Bucket: BUCKET_NAME }))
+      await s3Client.send(
+        new CreateBucketCommand({
+          Bucket: BUCKET_NAME,
+        }),
+      )
       return
     }
+
     throw error
   }
 }
