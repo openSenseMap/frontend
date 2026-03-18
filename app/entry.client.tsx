2e297ec7839c70a027d6a9f3e53ddef6b4bd1957
David Scheidt <david.scheidt@opensenselab.org>
2026-03-18 13:09:44 +0100

    feat: get everything to work with cookies

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -10,21 +10,15 @@ import i18nextOptions from './i18next-options'
 
 const hydrate = async () => {
 	await i18next
-		.use(initReactI18next) // Tell i18next to use the react-i18next plugin
-		.use(I18nextBrowserLanguageDetector) // Setup a client-side language detector
-		.use(I18NextHttpBackend) // Setup your backend
+		.use(initReactI18next)
+		.use(I18NextHttpBackend)
+		.use(I18nextBrowserLanguageDetector)
 		.init({
-			...i18nextOptions, //spread the configuration
-			// This function detects the namespaces your routes rendered while SSR us
+			...i18nextOptions,
 			ns: getInitialNamespaces(),
-			backend: { loadPath: '/locales/{{lng}}//{{ns}}.json' },
+			backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' },
 			detection: {
-				// Here only enable htmlTag detection, we'll detect the language only
-				// server-side with remix-i18next, by using the `<html lang>` attribute
-				// we can communicate to the client the language detected server-side
 				order: ['htmlTag'],
-				// Because we only use htmlTag, there's no reason to cache the language
-				// on the browser, so we disable it
 				caches: [],
 			},
 		})
--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import tailwindStylesheetUrl from '/app/styles/tailwind.css?url'
 import appStylesheetUrl from '/app/styles/app.css?url'
 import clsx from 'clsx'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
 	data,
@@ -9,16 +10,18 @@ import {
 	Outlet,
 	Scripts,
 	ScrollRestoration,
-	useLoaderData,
 	type LoaderFunctionArgs,
 	type MetaFunction,
 } from 'react-router'
-import { useChangeLanguage } from 'remix-i18next/react'
+import { type Route } from './+types/root'
 import { Toaster } from './components/ui/toaster'
 import { i18nCookie } from './cookies'
 import i18next from './i18next.server'
+import { i18nextMiddleware } from './middleware/i18next.server'
 import { getEnv } from './utils/env.server'
 import { getUser } from './utils/session.server'
+
+export const middleware = [i18nextMiddleware]
 
 export const links = () => {
 	return [
@@ -65,14 +68,11 @@ export const meta: MetaFunction = () => [
 export async function loader({ request }: LoaderFunctionArgs) {
 	const locale = await i18next.getLocale(request)
 	const user = await getUser(request)
-	// const themeSession = await getThemeSession(request);
-	// const { getTheme } = await themeSessionResolver(request);
 	return data(
 		{
 			user: user,
 			locale: locale,
 			ENV: getEnv(),
-			// theme: getTheme(),
 		},
 		{
 			headers: { 'Set-Cookie': await i18nCookie.serialize(locale) },
@@ -80,38 +80,22 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	)
 }
 
-export let handle = {
-	// In the handle export, we can add a i18n key with namespaces our route
-	// will need to load. This key can be a single string or an array of strings.
-	// TIP: In most cases, you should set this to your defaultNS from your i18n config
-	// or if you did not set one, set it to the i18next default namespace "translation"
-	i18n: 'common',
-}
-
-export default function AppWithProviders() {
-	// const data = useLoaderData<typeof loader>();
-
-	return (
-		// <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
-		<App />
-		// </ThemeProvider>
-	)
-}
-
-export function App() {
-	const data = useLoaderData<typeof loader>()
-	// const [theme] = useTheme();
-
-	let { i18n } = useTranslation()
-
-	// This hook will change the i18n instance language to the current locale
-	// detected by the loader, this way, when we do something to change the
-	// language, this locale will change and i18next will load the correct
-	// translation files
-	useChangeLanguage(data.locale)
+export default function App({
+	loaderData: { locale, ENV },
+}: Route.ComponentProps) {
+	const { i18n } = useTranslation()
+	useEffect(() => {
+		if (i18n.language !== locale) {
+			void i18n.changeLanguage(locale)
+		}
+	}, [locale, i18n])
 
 	return (
-		<html lang={data.locale} dir={i18n.dir()} className={clsx('light h-full')}>
+		<html
+			lang={i18n.language}
+			dir={i18n.dir(i18n.language)}
+			className={clsx('light h-full')}
+		>
 			<head>
 				<Meta />
 				{/* <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} /> */}
@@ -124,7 +108,7 @@ export function App() {
 				<Scripts />
 				<script
 					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+						__html: `window.ENV = ${JSON.stringify(ENV)}`,
 					}}
 				/>
 			</body>
