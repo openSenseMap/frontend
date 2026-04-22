8895185ef2c7efd8d64284a7d9b392f1351d64b1
David Scheidt <david.scheidt@opensenselab.org>
2026-04-22 10:57:36 +0200

    refactor: move types to env in app

--- app/env.d.ts
+++ app/env.d.ts
@@ -1,3 +1,6 @@
+/**
+ * Type declarations for the entire project.
+ **/
 declare module '@sensebox/sketch-templater' {
 	const SketchTemplater: new (cfg: unknown) => {
 		generateSketch: (box: unknown, options?: { encoding?: string }) => string
