e165f30e87fe674c71bd03642183426cbe250e9c
jona159 <raabejonas@yahoo.de>
2026-05-04 14:34:19 +0200

    feat: show logo and stats together

--- app/components/header/home/index.tsx
+++ app/components/header/home/index.tsx
@@ -1,28 +1,55 @@
 import { Link } from 'react-router'
+import { useTranslation } from 'react-i18next'
+
 const HOME_HASH = '#2.08/42.99/31.31'
 
-export default function Home() {
+interface HomeProps {
+	deviceCount?: number
+	measurementCount?: number
+}
+
+export default function Home({ deviceCount = 0, measurementCount = 0 }: HomeProps) {
+	const { t } = useTranslation('menu')
+
 	return (
-		<div>
-			<div className="pointer-events-auto h-10 w-40">
-				<Link
-					to={{
-						pathname: '/explore',
-						hash: HOME_HASH,
-					}}
-				>
-					<button
-						type="button"
-						className="rounded-full border border-gray-100 bg-white/90 text-black shadow-xl hover:bg-gray-100"
-					>
-						<img
-							src="/img/openSenseMap.png"
-							alt="openSenseMapLogo"
-							className="mx-auto"
-						/>
-					</button>
-				</Link>
-			</div>
-		</div>
+		<Link
+			to={{
+				pathname: '/explore',
+				hash: HOME_HASH,
+			}}
+			aria-label="Go to explore map"
+			className="
+				pointer-events-auto
+				flex h-11 items-center gap-3
+				rounded-full border border-gray-100
+				bg-white/90 px-3 pr-4
+				text-black shadow-xl backdrop-blur-md
+				transition hover:bg-gray-100
+			"
+		>
+			<img
+				src="/img/openSenseMap.png"
+				alt="openSenseMapLogo"
+				className="h-7 w-auto shrink-0"
+			/>
+
+			{deviceCount > 0 && (
+				<section className="flex flex-col leading-tight text-sm">
+					<p>
+						<span className="font-semibold text-green-700">
+							{deviceCount}{' '}
+						</span>
+						<span>{t('devices')}</span>
+					</p>
+
+					<p>
+						<span className="font-semibold text-green-700">
+							{measurementCount}{' '}
+						</span>
+						<span>{t('measurements')}</span>
+					</p>
+				</section>
+			)}
+		</Link>
 	)
 }
