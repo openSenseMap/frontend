fa2dc873fb5ad74cca4fe34c7fddccd3d7a9f14d
jona159 <raabejonas@yahoo.de>
2026-05-06 11:20:12 +0200

    feat: wrap in tooltip provider

--- app/components/map/topbar.tsx
+++ app/components/map/topbar.tsx
@@ -3,6 +3,7 @@ import Menu from '../header/menu'
 import NavBar from '../header/nav-bar'
 import Info from '../header/info'
 import LanguageSelector from '../landing/header/language-selector'
+import { TooltipProvider } from '../ui/tooltip'
 
 interface MapHeaderProps {
 	user: any
@@ -17,6 +18,7 @@ export default function MapHeader({
 	onHomeClick
 }: MapHeaderProps) {
 	return (
+		<TooltipProvider>
 		<header className="pointer-events-none absolute top-0 left-0 z-20 w-full">
 			<div className="pointer-events-auto flex min-h-14 w-full items-start justify-between gap-4 px-3 py-2">
 				<div className="flex items-center gap-3">
@@ -28,19 +30,22 @@ export default function MapHeader({
 
 				</div>
 
-				<div className="min-w-0 flex-1">
+				<div className="flex min-w-0 flex-1 justify-center">
 					<NavBar devices={devices} />
 				</div>
 
-				<LanguageSelector />
-				<div className="flex shrink-0 items-center gap-2 md:ml-14">
+				<div className="flex shrink-0 items-center gap-4">
+					<LanguageSelector />
+					
+					<div className="h-7 w-px bg-black/10 dark:bg-white/15" />
 
-					<Info />
-					{/* TODO: add settings menu once we have theme, more languages etc. */}
-					{/* {user && <Settings />} */}
-					<Menu devices={devices} />
+					<div className="flex items-center gap-2">
+						<Info />
+						<Menu devices={devices} />
+					</div>
 				</div>
 			</div>
 		</header>
+		</TooltipProvider>
 	)
 }
