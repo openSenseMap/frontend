a1d9b3701027210797bcaa27e2275ce8c8392ea8
jona159 <raabejonas@yahoo.de>
2026-06-01 14:58:10 +0200

    feat: add content type middlewares

--- app/middleware/content-type-header.server.ts
+++ app/middleware/content-type-header.server.ts
@@ -5,15 +5,26 @@ import { StandardResponse } from '~/lib/responses'
  * A middleware function responding with HTTP 415 Unsupported Media Type
  * to requests that do not contain the Content-Type: application/json header.
  */
-export const requestContentTypeJson: MiddlewareFunction<Response> = ({
-	request,
-}) => {
-	const contentType = request.headers.get('content-type') || ''
-	if (!contentType.includes('application/json'))
-		return StandardResponse.unsupportedMediaType(
-			'Unsupported content-type. Try application/json',
-		)
-}
+export const requestContentTypeJson =
+	(methods?: string[]): MiddlewareFunction<Response> =>
+	({ request }) => {
+		const allowedMethods = methods?.map((method) => method.toUpperCase())
+
+		if (
+			allowedMethods &&
+			!allowedMethods.includes(request.method.toUpperCase())
+		) {
+			return
+		}
+
+		const contentType = request.headers.get('content-type') || ''
+
+		if (!contentType.includes('application/json')) {
+			return StandardResponse.unsupportedMediaType(
+				'Unsupported content-type. Try application/json',
+			)
+		}
+	}
 
 /**
  * A middleware function that sets the Content-Type: application/json
@@ -27,3 +38,46 @@ export const responseContentTypeJson: MiddlewareFunction<Response> = async (
 	res.headers.set('Content-Type', 'application/json; charset=utf-8')
 	return res
 }
+
+/**
+ * A middleware function responding with HTTP 415 Unsupported Media Type
+ * to requests that do not contain the Content-Type application/x-www-form-urlencoded
+ * or Content-Type multipart/form-data header.
+ */
+export const requestContentTypeForm: MiddlewareFunction<Response> = ({
+	request,
+}) => {
+	const contentType = request.headers.get('content-type') || ''
+
+	if (
+		!contentType.includes('application/x-www-form-urlencoded') &&
+		!contentType.includes('multipart/form-data')
+	) {
+		return StandardResponse.unsupportedMediaType(
+			'Unsupported content-type. Try application/x-www-form-urlencoded',
+		)
+	}
+}
+
+export const requestContentTypeJsonOrForm =
+	(methods?: string[]): MiddlewareFunction<Response> =>
+	({ request }) => {
+		const allowedMethods = methods?.map((method) => method.toUpperCase())
+
+		if (
+			allowedMethods &&
+			!allowedMethods.includes(request.method.toUpperCase())
+		) {
+			return
+		}
+		const contentType = request.headers.get('content-type') || ''
+
+		if (
+			!contentType.includes('application/json') &&
+			!contentType.includes('application/x-www-form-urlencoded')
+		) {
+			return StandardResponse.unsupportedMediaType(
+				'Unsupported content-type. Try application/json or application/x-www-form-urlencoded',
+			)
+		}
+	}
