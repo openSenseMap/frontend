91c2c3f1083922913c34315465df6f3d14168f7e
jona159 <raabejonas@yahoo.de>
2026-07-08 12:34:58 +0200

    feat: admin dashboard for rate limits

--- app/routes/admin._index.tsx
+++ app/routes/admin._index.tsx
@@ -12,6 +12,9 @@ export default function AdminIndexRoute() {
 				<div className="rounded border p-4">
 					<Link to="/admin/devices">Edit devices</Link>
 				</div>
+				<div className="rounded border p-4">
+					<Link to="/admin/rate-limits">Edit rate limits</Link>
+				</div>
 			</div>
 		</div>
 	)
