231b0c39be511530b944f59e258bdf03a85e014c
jona159 <raabejonas@yahoo.de>
2026-08-24 11:27:37 +0200

    fix: rm unnecessary custom data attribute

--- app/components/landing/header/header.tsx
+++ app/components/landing/header/header.tsx
@@ -87,7 +87,6 @@ export default function Header() {
 						<button
 							id="navbar-menu-toggle"
 							onClick={() => setOpenMenu(!openMenu)}
-							data-collapse-toggle="navbar-mobile-menu"
 							type="button"
 							className="inline-flex size-11 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 focus:outline-hidden lg:hidden"
 							aria-controls="navbar-mobile-menu"
