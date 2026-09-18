f448576d5e93288658887bf0a77c06b9b3ca12c0
jona159 <raabejonas@yahoo.de>
2026-09-18 11:36:44 +0200

    feat: load device location history for mobile devices

--- app/routes/explore.$deviceId.tsx
+++ app/routes/explore.$deviceId.tsx
@@ -4,7 +4,7 @@ import { type Route } from './+types/explore.$deviceId'
 import DeviceDetailBox from '~/components/device-detail/device-detail-box'
 import { HoveredPointContext } from '~/components/map/layers/mobile/mobile-box-layer'
 import MobileOverviewLayer from '~/components/map/layers/mobile/mobile-overview-layer'
-import { getDevice } from '~/db/models/device.server'
+import { getDevice, getDeviceLocations } from '~/db/models/device.server'
 import { getSensorsWithLastMeasurement } from '~/db/models/sensor.server'
 import { categorizeIntoTrips } from '~/lib/mobile-box-helper'
 import { getDeviceImageUrl } from '~/lib/s3.server'
@@ -19,10 +19,15 @@ export async function loader({ context, params, request }: Route.LoaderArgs) {
 		throw new Response('Device not found', { status: 502 })
 	}
 
-	const device = await getDevice({ id: params.deviceId })
-	const sensorsWithLastestMeasurement = await getSensorsWithLastMeasurement(
-		params.deviceId,
-	)
+	const [loadedDevice, sensorsWithLastestMeasurement] = await Promise.all([
+		getDevice({ id: params.deviceId }),
+		getSensorsWithLastMeasurement(params.deviceId),
+	])
+	const locations =
+		loadedDevice?.exposure === 'mobile'
+			? await getDeviceLocations({ id: params.deviceId })
+			: []
+	const device = loadedDevice ? { ...loadedDevice, locations } : loadedDevice
 
 	// get only locations from the last 5 trips
 	if (device?.exposure === 'mobile' && device?.locations) {
